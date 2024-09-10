037bbfdc9aa5093b888bab2b10d09b46d5dc2819
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-11 00:48:28 +0100

    feat: login!

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
-export default function LoginPage() {
+import LoginForm from "@/components/auth/login"
+
+export default function SignupPage() {
   return (
-    <div className="container text-white">
-      <h1>Login</h1>
+    <div className="container text-white h-screen flex flex-col items-center justify-center py-20">
+      <h1 className="font-bold text-3xl">Login page</h1>
+      <LoginForm />
     </div>
   )
 }
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -15,7 +15,7 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod"
 import { useMutation } from "@tanstack/react-query"
-import { signUp } from "@/actions/user/signup"
+import { login } from "@/actions/user/login"
 import { InputWithLabel } from "../ui/input-label"
 import { toast } from "sonner"
 import { useRouter } from "next/navigation"
@@ -33,7 +33,25 @@ export default function LoginForm() {
     }
   })
 
-  const handleLogin = (values: SchemaProps) => { }
+  const { 
+    data,
+    mutateAsync: server_login,
+    isPending
+  } = useMutation({
+    mutationFn: (values: SchemaProps) => login(values.email, values.password),
+    onSuccess: () => {
+      // show success toast
+      toast.success("Login successful!")
+      // redirect to dashboard
+      router.push("/dashboard")
+    },
+    onError: (error) => {
+      // show error toast
+      toast.error(error.message)
+    }
+  })
+
+  const handleLogin = (values: SchemaProps) => server_login(values)
 
   return (
     <Form {...form}>
@@ -41,8 +59,47 @@ export default function LoginForm() {
         onSubmit={form.handleSubmit(handleLogin)}
         className="grid grid-cols-12 gap-4 w-96 mt-8"
       >
-
-
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormControl>
+              <div className="col-span-6">
+                <InputWithLabel
+                  label="Email"
+                  type="email"
+                  {...field}
+                />
+                <FormMessage>{form.formState?.errors?.email?.message}</FormMessage>
+              </div>
+            </FormControl>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormControl>
+              <div className="col-span-6">
+                <InputWithLabel
+                  label="Password"
+                  type="password"
+                  {...field}
+                />
+                <FormMessage>{form.formState?.errors?.password?.message}</FormMessage>
+              </div>
+            </FormControl>
+          )}
+        />
+        <FormItem className="col-span-full">
+          <Button
+            type="submit"
+            disabled={isPending}
+            className="w-full"
+          >
+            {isPending ? "Loading..." : "Login"}
+          </Button>
+        </FormItem>
       </form>
     </Form>
   )

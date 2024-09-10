4d255c2ca4aad5de111b53a30e5ca0636656b852
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-11 00:13:29 +0100

    feat: base dirs, shadcn, zod

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="container text-white">
+      <h1>Login</h1>
+    </div>
+  )
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <div className="container text-white">
+      <h1>Sign up page</h1>
+    </div>
+  )
+}

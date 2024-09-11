fae150fa14c6bab86b0ac6b0b156b64956593bde
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-11 22:45:49 +0100

    feat: start of admin middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { updateSession } from "@/utils/supabase/middleware";
+
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/admin/:path*',
+}
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  // get the current user that is trying to access the admin panel
+  const { user: user } = await updateSession(request);
+
+  // early exit if we do not find the user
+  if (!user || !user.user?.id) {
+    // redirect them to the signup page
+    return NextResponse.redirect('/signup?r=no-user');
+  }
+
+  // the user level is going to have to be checked on the admin page. 
+  // we can just pass the user object to the page
+
+  // run next here to prevent infinite loop
+  return NextResponse.next();
+}

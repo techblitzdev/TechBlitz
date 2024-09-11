a1cfc7620a84f06623ab6da09a10117ff0fabdd0
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-11 22:47:48 +0100

    feat: adds error-code constants

---
+++ src/types/Constants.ts
@@ -0,0 +1,11 @@
+type ErrorDetail = {
+  title: string;
+  description?: string;
+};
+
+export type ErrorCodes = {
+  "not-authenticated": ErrorDetail;
+  "unauthorized": string | ErrorDetail;
+  "no-user": ErrorDetail;
+};
+

4f8529bfd9f243da9d183d1ab7fc998ca0cb69e0
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-13 17:00:21 +0100

    linting!

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,9 @@
-'use client'
-import SignupForm from '@/components/auth/signup'
+'use client';
+
+import SignupForm from '@/components/auth/signup';
 import { ERROR_CODES } from '@/utils/constants/error-codes';
-import { useGetQueryParams } from '@/utils/get-query-params'
-import { useRef, useState } from 'react';
+import { useGetQueryParams } from '@/utils/get-query-params';
+import { Suspense, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import type { ErrorCodes } from '@/types/Constants'; // Import the ErrorCodes type
 
@@ -10,19 +11,28 @@ export default function SignupPage() {
   const ranToast = useRef(false);
   const [params, setUrlParams] = useState({});
 
+  console.log(params);
 
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignupContent ranToast={ranToast.current} setUrlParams={setUrlParams} />
+    </Suspense>
+  );
+}
+
+function SignupContent({ ranToast, setUrlParams }: { ranToast: boolean; setUrlParams: any }) {
   // check if we have any query parameters
   const urlParams = useGetQueryParams({
-    keys: ['r']
+    keys: ['r'],
   });
 
   // if we have a query parameter, we can use it to display a toast message
   if (
     Object.keys(urlParams).length > 0 &&
     urlParams.r === 'no-user' &&
-    !ranToast.current
-  ) {  
-    ranToast.current = true;
+    !ranToast
+  ) {
+    ranToast = true;
 
     // Cast the query parameter to one of the valid keys in ErrorCodes
     const firstError = urlParams.r as keyof ErrorCodes;
@@ -46,8 +56,6 @@ export default function SignupPage() {
       });
     }
   }
-
-  console.log(JSON.stringify(params));
 
   return (
     <div className="container text-white h-screen flex flex-col items-center justify-center py-20">

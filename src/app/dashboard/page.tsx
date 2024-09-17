cf40e67d16c2deec3cce884914df9fdcf87ddd33
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-17 22:16:42 +0100

    fix build

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,18 +2,18 @@ import { getUserFromDb, getUserFromSession } from '@/actions/user/get-user';
 
 export default async function Dashboard() {
   // get our current user
-  const user = await getUserFromSession();
-  if (!user || !user?.data?.user.id) return;
+  const { data: user } = await getUserFromSession();
+  if (!user || !user.user?.id) return;
 
   // get the user from the db
-  const userData = await getUserFromDb(user.data.user.id);
+  const userData = await getUserFromDb(user.user.id);
 
   console.log('userData', userData);
 
   return (
     <div className="container text-white">
       <h1 className="font-bold text-3xl">Dashboard</h1>
-      <p>UserId: {user.data.user?.id}</p>
+      <p>UserId: {user.user?.id}</p>
       <p>User email: {userData?.email}</p>
       <p>User role: {userData?.userLevel}</p>
     </div>

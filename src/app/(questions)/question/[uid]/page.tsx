55b17c1fad5f5feaf45d7d5ec07a0499c7d94f81
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-26 13:03:37 +0100

    chore: better initialising of prisma client

--- src/app/(questions)/question/[uid]/page.tsx
+++ src/app/(questions)/question/[uid]/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+import { useState } from 'react';
 import { getQuestion } from '@/actions/questions/get';
+import { answerQuestion } from '@/actions/answers/answer';
+import { useQuery } from '@tanstack/react-query';
+
+// components
 import { Button } from '@/components/ui/button';
 
-export default async function TodaysQuestionPage({
+export default function TodaysQuestionPage({
   params,
 }: {
   params: {
@@ -9,27 +15,86 @@ export default async function TodaysQuestionPage({
   };
 }) {
   const { uid } = params;
+  // State to store the selected answer UID
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
 
-  const question = await getQuestion(uid);
+  // Fetch the question data
+  const {
+    data: question,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['question', uid],
+    queryFn: async () => {
+      const data = await getQuestion(uid);
+      return data;
+    },
+  });
+
   if (!question) return <p>Loading...</p>;
 
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  // Handle answer selection
+  const handleSelectAnswer = (answerUid: string) => {
+    setSelectedAnswer(answerUid);
+  };
+
+  // Handle form submission
+  const handleAnswerQuestion = async () => {
+    if (!selectedAnswer) {
+      alert('Please select an answer.');
+      return;
+    }
+
+    const isCorrect = await answerQuestion({
+      questionUid: uid,
+      answerUid: selectedAnswer,
+    });
+
+    console.log({
+      isCorrect,
+    });
+  };
+
   return (
-    <form className="font-inter flex flex-col gap-y-4">
+    <form
+      className="font-inter flex flex-col gap-y-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleAnswerQuestion();
+      }}
+    >
       {/** Question title */}
       <h1 className="font-semibold font-inter text-3xl">
         {question?.question}
       </h1>
-      {/** Checkbox of answers */}
+
+      {/** List of answers */}
       <div className="flex flex-col gap-y-2">
         {question?.answers.map((answer) => (
           <div key={answer.uid} className="flex items-center gap-x-2">
-            <input type="checkbox" />
+            <input
+              type="radio"
+              name="answer"
+              value={answer.uid}
+              checked={selectedAnswer === answer.uid}
+              onChange={() => handleSelectAnswer(answer.uid)}
+            />
             <span>{answer.answer}</span>
           </div>
         ))}
       </div>
+
       {/** Submit button */}
-      <Button variant="default" type="submit">
+      <Button type="submit" variant="default">
         Submit
       </Button>
     </form>

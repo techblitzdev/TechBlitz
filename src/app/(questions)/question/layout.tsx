674190739bc2fe0aab1a14905da7c7bcc0a34f29
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-28 18:54:35 +0100

    feat: adds satoshi fonts & starting to use it

--- src/app/(questions)/question/layout.tsx
+++ src/app/(questions)/question/layout.tsx
@@ -5,7 +5,7 @@ export default function QuestionsLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="container text-white flex flex-col gap-y-4 py-12">
-      <div className="flex w-full justify-between items-center font-inter">
+      <div className="flex w-full justify-between items-center font-satoshi">
         <h1 className="font-bold text-lg">Daily Question</h1>
         <span>0:00</span>
       </div>
--- src/app/styles/fonts/font.ts
+++ src/app/styles/fonts/font.ts
@@ -1,9 +1,17 @@
 import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const InterFont = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
   style: 'normal',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const SatoshiFont = localFont({
+  src: './satoshi/Satoshi-Variable.ttf',
+  variable: '--font-satoshi',
+  style: 'normal',
+  weight: '100 200 300 400 500 600 700 800 900',
 });

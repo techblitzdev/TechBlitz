988e25801a413f474018553b5a91e3996af5a1c5
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-09-28 18:33:23 +0100

    feat: added react countup & using on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,36 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import LoadingSpinner from '@/components/ui/loading';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import CountUp from 'react-countup';
 
 export default function NotFound() {
   const router = useRouter();
-  const goBack = () => router.push('/dashboard');
+  const goBack = () => {
+    setLoading(true);
+    router.push('/dashboard');
+  };
+
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="w-full flex items-center justify-center min-h-screen bg-dot-white/[0.2] relative">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
       <div className="flex flex-col text-center gap-x-5 items-center font-inter">
-        <h2 className="text-8xl font-semibold">404</h2>
+        <CountUp
+          end={404}
+          className="min-h-24 text-8xl font-semibold"
+          duration={2}
+        />
         <div className="flex flex-col max-w-96">
           <p className="text-sm">
             Sorry, it look's like the page you have requested could not be
             found.
           </p>
           <Button variant="secondary" className="mt-4" onClick={goBack}>
-            Back to dashboard
+            {loading ? <LoadingSpinner /> : 'Back to dashboard'}
           </Button>
         </div>
       </div>

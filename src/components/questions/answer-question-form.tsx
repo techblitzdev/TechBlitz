0534267b34c016252dab21e3f8b6f1f069f866a0
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-10-04 21:37:09 +0100

    feat: progress with answer question structure & modal

--- src/components/questions/answer-question-form.tsx
+++ src/components/questions/answer-question-form.tsx
@@ -4,14 +4,16 @@ import { answerQuestion } from '@/actions/answers/answer';
 // components
 import { Button } from '../ui/button';
 import { Form, FormControl, FormField } from '@/components/ui/form';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 // zod
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { answerQuestionSchema } from '@/lib/zod/schemas/answer-question-schema';
 // types
-import { Question } from '@/types/Questions';
+import type { Question } from '@/types/Questions';
 import type { User } from '@supabase/supabase-js';
+import AnswerQuestionModal from './answer-question-modal';
 
 type SchemaProps = z.infer<typeof answerQuestionSchema>;
 type AnswerQuestionFormProps = {
@@ -28,12 +30,13 @@ export default function AnswerQuestionForm({
   const [correctAnswer, setCorrectAnswer] = useState<
     'init' | 'incorrect' | 'correct'
   >('init');
+  const [openAnswerModal, setOpenAnswerModal] = useState(false);
 
   const buttonText = useCallback(() => {
-    if (correctAnswer === null) {
+    if (correctAnswer === 'init') {
       return 'Submit';
     }
-    return correctAnswer ? 'Correct!' : 'Incorrect!';
+    return correctAnswer === 'correct' ? 'Correct!' : 'Incorrect!';
   }, [correctAnswer]);
 
   /** FORM */
@@ -56,47 +59,60 @@ export default function AnswerQuestionForm({
     });
 
     setCorrectAnswer(isCorrect ? 'correct' : 'incorrect');
+    setOpenAnswerModal(true);
   };
 
   return (
-    <Form {...form}>
-      <form
-        className="font-satoshi flex flex-col gap-y-4"
-        onSubmit={form.handleSubmit(handleAnswerQuestion)}
-      >
-        <h1 className="font-semibold font-inter text-3xl">
-          {question.question}
-        </h1>
-        <div className="flex flex-col gap-y-2">
-          {question.answers.map((answer) => (
-            <FormField
-              control={form.control}
-              name="answer"
-              key={answer.uid}
-              render={({ field }) => (
-                <FormControl>
-                  <label className="flex items-center gap-x-2">
-                    <input
-                      {...field}
-                      type="radio"
-                      name="answer"
-                      value={answer.uid}
-                      checked={field.value === answer.uid}
-                      onChange={() => {
-                        field.onChange(answer.uid);
-                      }}
-                    />
-                    <span>{answer.answer}</span>
-                  </label>
-                </FormControl>
-              )}
-            />
-          ))}
-        </div>
-        <Button type="submit" variant="default">
-          {buttonText()}
-        </Button>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form
+          className="font-satoshi flex flex-col gap-y-4"
+          onSubmit={form.handleSubmit(handleAnswerQuestion)}
+        >
+          <h1 className="font-semibold font-inter text-3xl">
+            {question.question}
+          </h1>
+          <div className="flex flex-col gap-y-2">
+            {question.answers.map((answer) => (
+              <FormField
+                control={form.control}
+                name="answer"
+                key={answer.uid}
+                render={({ field }) => (
+                  <FormControl>
+                    <label className="flex items-center gap-x-2">
+                      <input
+                        {...field}
+                        type="radio"
+                        name="answer"
+                        value={answer.uid}
+                        checked={field.value === answer.uid}
+                        onChange={() => {
+                          field.onChange(answer.uid);
+                        }}
+                      />
+                      <span>{answer.answer}</span>
+                    </label>
+                  </FormControl>
+                )}
+              />
+            ))}
+          </div>
+          <Dialog>
+            <DialogTrigger>
+              <Button type="submit" variant="default">
+                {buttonText()}
+              </Button>
+              <AnswerQuestionModal
+                question={question}
+                user={userData}
+                correct={correctAnswer}
+                isOpen={openAnswerModal}
+              />
+            </DialogTrigger>
+          </Dialog>
+        </form>
+      </Form>
+    </>
   );
 }
--- src/components/questions/answer-question-modal.tsx
+++ src/components/questions/answer-question-modal.tsx
@@ -5,12 +5,33 @@ import {
   DialogContent,
   DialogDescription,
   DialogFooter,
+  DialogTitle,
 } from '@/components/ui/dialog';
 
+import type { Question } from '@/types/Questions';
+import type { User } from '@supabase/supabase-js';
+
 type AnswerQuestionModalProps = {
-  questionUid: string;
+  isOpen: boolean;
+  question: Question;
+  user: User;
+  correct: 'correct' | 'incorrect' | 'init';
 };
 
-export default function AnswerQuestionModal() {
-  return <Dialog></Dialog>;
+export default function AnswerQuestionModal({
+  question,
+  user,
+  correct,
+}: AnswerQuestionModalProps) {
+  return (
+    <DialogContent className="bg-black-75 md:max-w-3xl">
+      <DialogHeader>
+        <DialogTitle>That was {correct}</DialogTitle>
+      </DialogHeader>
+
+      <DialogDescription>{JSON.stringify(question)}</DialogDescription>
+
+      <DialogFooter></DialogFooter>
+    </DialogContent>
+  );
 }

737791b565b3118654b89003da88a941b70eaeb1
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-10-21 22:05:06 +0100

    feat: adds base Stripe util

---
+++ src/lib/stripe.ts
@@ -0,0 +1,9 @@
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+export const getStripe = async (): Promise<Stripe | null> => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY as string);
+  }
+  return stripePromise;
+};

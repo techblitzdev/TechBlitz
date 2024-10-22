b7a44cac0e21674fcd122691a326becae722efbc
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-10-22 22:55:47 +0100

    fix: ... rolled the key and set a new one smh

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(
-  'sk_test_51QCSYxCX23ptLp4LDRAVgbPzuOFS7s7ukSS7c2aql4kbKfCxkVNdWwYvS2BqENlbNWtBPnaD58FiPXGGiFC00Nsi00naFJwqlA',
+  process.env.NEXT_PRIVATE_STRIPE_SECRET_KEY as string,
   {
     apiVersion: '2024-09-30.acacia',
   }

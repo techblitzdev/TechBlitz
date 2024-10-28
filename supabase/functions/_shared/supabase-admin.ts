d478ba259a2c2ffd45250b4bf6b8cb192f0d23a5
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-10-28 20:27:35 +0000

    build

--- supabase/functions/_shared/supabase-admin.ts
+++ supabase/functions/_shared/supabase-admin.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'npm:@supabase/supabase-js@2'
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   Deno.env.get('SUPABASE_URL'),

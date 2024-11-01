cec6e1e426127ffc63918e44daab31e881de7869
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-01 16:47:40 +0000

    build

--- src/app/api/auth/update/route.ts
+++ src/app/api/auth/update/route.ts
@@ -1,3 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) { }
+export async function GET(req: NextRequest) { 
+  return NextResponse.json({ message: "Hello, World!" });
+}

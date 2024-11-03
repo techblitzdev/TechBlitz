ccd14078a972440239ee0f156fb79b37e6941654
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-03 15:37:44 +0000

    chore(cleanup): some minor code cleanup in login form & login action

--- src/app/(no_nav)/(auth)/layout.tsx
+++ src/app/(no_nav)/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({
 }>) {
   return (
     <div className="relative bg-dot-white/[0.2]">
-      <h6 className="top-4 left-4 absolute font-satoshi text-2xl z-30">
+      <h6 className="font-semibold top-4 left-4 absolute font-satoshi text-2xl z-30">
         meerge
       </h6>
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>

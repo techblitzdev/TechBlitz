b45de3a62d93fc2bcabb0f0d7c83ff3163422b80
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-05 19:14:39 +0000

    feat(component): adds accordion component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
   			}
   		},
   		container: {
-  			center: true,
+  			center: 'true',
   			padding: '2rem',
   			screens: {
   				'2xl': '1400px'
@@ -97,6 +97,28 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },

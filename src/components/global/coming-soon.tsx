de3840388dc02af1c9bdbfcf11a09cbf5e87e340
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-07 12:18:34 +0000

    minor style change to coming soon chip

--- src/components/global/coming-soon.tsx
+++ src/components/global/coming-soon.tsx
@@ -1,7 +1,7 @@
 export default function ComingSoonChip() {
   return (
     <div className="bg-accent px-2 py-0.5 rounded-md text-black text-center">
-      <p className="text-[10px] font-semibold">Coming soon</p>
+      <p className="text-[10px] font-satoshi font-medium">Coming soon</p>
     </div>
   );
 }

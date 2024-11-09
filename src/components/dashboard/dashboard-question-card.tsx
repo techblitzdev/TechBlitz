c4246ec263a8823330a30fb0943d17ab78c516b0
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-09 20:23:03 +0000

    feat(dashboard): adds a strikethrough q's that are complete

--- src/components/dashboard/dashboard-question-card.tsx
+++ src/components/dashboard/dashboard-question-card.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon } from '@radix-ui/react-icons';
+import { Check } from 'lucide-react';
 import { X } from 'lucide-react';
 
 export default function DashboardQuestionCard({
@@ -6,20 +6,23 @@ export default function DashboardQuestionCard({
 }: {
   question: string;
 }) {
-  const randomQuestion = Math.random() > 0.5;
+  const isComplete = Math.random() > 0.5;
 
   return (
     <div className="bg-black-100 border border-black-50 rounded-md p-4 mb-4">
       <div className="flex items-center">
         <div className="flex items-center justify-center size-4 border rounded-sm border-black-50 mr-3">
-          {/** randomly add a tick icon */}
-          {randomQuestion ? (
-            <CheckIcon className="w-4 h-4 text-green-500" />
+          {isComplete ? (
+            <Check className="w-4 h-4 text-green-500" />
           ) : (
             <X className="w-4 h-4 text-red-500" />
           )}
         </div>
-        <span className="text-sm font-ubuntu">{question}</span>
+        <span
+          className={`text-sm font-ubuntu ${isComplete ? 'line-through' : ''}`}
+        >
+          {question}
+        </span>
       </div>
     </div>
   );

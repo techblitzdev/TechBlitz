1c75e621e3299b0f7dc0686e6f958f4fb3eb8168
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-12 17:25:47 +0000

    chore: adds spotlight for local dev hydration error debugging

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const {
+  withHydrationOverlay,
+} = require("@builder.io/react-hydration-overlay/next");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -11,3 +15,11 @@ module.exports = {
     ],
   },
 }
+
+module.exports = withHydrationOverlay({
+  /**
+   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+   */
+  appRootSelector: "main",
+})(nextConfig);

f13eee8b7bea9b4adf0d33e48ec3408b7072c853
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-16 18:39:20 +0000

    feat(billing): new util to fetch user invoices

---
+++ src/actions/stripe/stripe-get-user-invoices.ts
@@ -0,0 +1,28 @@
+'use server';
+import { stripe } from '@/lib/stripe';
+import { prisma } from '@/utils/prisma';
+
+export const getUserInvoices = async (userUid: string) => {
+  // get the user's subscripton object
+  const subscription = await prisma.subscriptions.findFirst({
+    where: {
+      userUid,
+    },
+  });
+
+  if (!subscription) {
+    return [];
+  }
+
+  const stripeCustomerId = subscription.stripeCustomerId;
+  if (!stripeCustomerId) {
+    return [];
+  }
+
+  const invoices = await stripe.invoices.list({
+    customer: stripeCustomerId,
+    limit: 10,
+  });
+
+  return invoices.data;
+};

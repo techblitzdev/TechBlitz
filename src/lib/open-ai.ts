122e3dd4913324b1f889942fcd57f3c819c841d9
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-21 21:42:16 +0000

    feat(ai): added test action to generate some dummy data!

---
+++ src/lib/open-ai.ts
@@ -0,0 +1,9 @@
+import OpenAI from 'openai';
+
+const apiKey = process.env.NEXT_PRIVATE_OPEN_AI_API_KEY;
+const organization = process.env.NEXT_PRIVATE_OPEN_AI_ORG;
+
+export const openai = new OpenAI({
+  apiKey,
+  organization,
+});

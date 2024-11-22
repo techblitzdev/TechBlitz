7e700369e0129eb2df577ef952ce2b5f5367671e
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-22 15:25:35 +0000

    feat(roadmap): new endpoint to convert question / answer data into a structure to send up

---
+++ src/actions/roadmap/ai/get-question-data-for-gen.ts
@@ -0,0 +1,61 @@
+'use server';
+import { prisma } from '@/utils/prisma';
+
+export const generateDataForAi = async (opts: {
+  roadmapUid: string;
+  userUid: string;
+}) => {
+  const { roadmapUid, userUid } = opts;
+
+  // get the roadmap and check if the roadmap has already been generated
+  // and if the user that is requesting the data is the same user that
+  // generated the data
+  const roadmap = await prisma.userRoadmaps.findUnique({
+    where: {
+      uid: roadmapUid,
+      AND: {
+        userUid,
+      },
+    },
+  });
+
+  if (roadmap?.hasGeneratedRoadmap) {
+    return 'Roadmap already generated';
+  }
+
+  // we need to get all of the answers that the user has answered
+  // for this roadmap
+  const roadmapDefaultAnswers =
+    await prisma.defaultRoadmapQuestionsUsersAnswers.findMany({
+      where: {
+        roadmapUid,
+      },
+    });
+
+  if (roadmapDefaultAnswers.length === 0) {
+    return 'No answers found';
+  }
+
+  // start an array to store the questions that we will be asking
+  const userAnswers = [];
+
+  // now go and get the questions the user answered
+  for (const [index, answer] of roadmapDefaultAnswers.entries()) {
+    const question = await prisma.defaultRoadmapQuestions.findUnique({
+      where: {
+        uid: answer.questionUid,
+      },
+    });
+
+    // push the question along with the user's answer
+    if (question) {
+      userAnswers.push({
+        question: question.question,
+        answer: answer.correct,
+        difficulty: question.difficulty,
+      });
+    }
+  }
+
+  return userAnswers;
+};
--- src/app/(default_layout)/(roadmap)/roadmap/[roadmapUid]/page.tsx
+++ src/app/(default_layout)/(roadmap)/roadmap/[roadmapUid]/page.tsx
@@ -1,4 +1,6 @@
+import { generateDataForAi } from '@/actions/roadmap/ai/get-question-data-for-gen';
 import { test } from '@/actions/roadmap/ai/test';
+import { useUserServer } from '@/hooks/useUserServer';
 
 export default async function RoadmapSinglgePage({
   params,
@@ -7,7 +9,20 @@ export default async function RoadmapSinglgePage({
 }) {
   const { roadmapUid } = params;
 
-  //const foo = await test();
+  const user = await useUserServer();
+  if (!user) {
+    return <div>Not logged in</div>;
+  }
 
-  return <div>Hello from {roadmapUid}</div>;
+  const foo = await generateDataForAi({
+    roadmapUid,
+    userUid: user.uid,
+  });
+
+  return (
+    <div className="px-6 space-y-10">
+      <div>Hello from {roadmapUid}</div>
+      <pre className="">{JSON.stringify(foo, null, 2)}</pre>
+    </div>
+  );
 }

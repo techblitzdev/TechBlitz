23b80e2698296acd0a34572d90e2dfecf6f457dc
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-22 23:11:49 +0000

    feat(roadmap): removes order schema from question answers

--- src/actions/roadmap/ai/utils/add-order-to-response-questions.ts
+++ src/actions/roadmap/ai/utils/add-order-to-response-questions.ts
@@ -1,15 +1,15 @@
 /**
  * Method to add order to response questions
+ *
+ * do not add a order to the response answers
  */
 export const addOrderToResponseQuestions = (response: any) => {
   if (!Array.isArray(response)) {
     throw new Error('Input must be an array of questions.');
   }
 
-  return response.map((question, index) => {
-    return {
-      ...question,
-      order: index + 1,
-    };
-  });
+  return response.map((question, index) => ({
+    ...question,
+    order: index,
+  }));
 };

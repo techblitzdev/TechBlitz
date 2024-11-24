5b4d0e3b409ad82e12c79c392434dcc87e9fa1b1
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-24 00:50:08 +0000

    first way of adding 'timeline' sidebar line to roadmap page

--- src/app/(default_layout)/(roadmap)/roadmap/[roadmapUid]/(roadmapUid)/page.tsx
+++ src/app/(default_layout)/(roadmap)/roadmap/[roadmapUid]/(roadmapUid)/page.tsx
@@ -9,6 +9,7 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { useUserServer } from '@/hooks/useUserServer';
+import { cn } from '@/utils/cn';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
@@ -33,15 +34,36 @@ export default async function RoadmapSinglgePage({
     userUid: user.uid,
   });
 
+  // order the questions via the order
+  generatedPlan.sort((a, b) => a.order - b.order);
+
   return (
     <div className="flex flex-col lg:flex-row gap-10 mt-5 container">
-      <div className="w-full lg:w-1/2">
+      <div className="w-full lg:w-1/2 relative flex gap-7 items-center">
+        <div className="relative flex flex-col items-center w-0.5 h-full">
+          {generatedPlan?.map((question, index) => (
+            <div
+              key={question.uid}
+              className={cn(
+                'w-full h-full',
+                question.completed ? 'bg-green-500' : 'bg-black-50'
+              )}
+              style={{
+                height:
+                  index < generatedPlan.length - 1
+                    ? 'calc(100% / (generatedPlan.length - 1))'
+                    : 'auto',
+              }}
+            ></div>
+          ))}
+        </div>
         <div className="space-y-6">
-          {generatedPlan?.map((step) => (
+          {generatedPlan?.map((question, index) => (
             <RoadmapQuestionCard
-              key={step.uid}
-              question={step}
+              key={question.uid}
+              question={question}
               roadmapUid={roadmapUid}
+              index={index}
             />
           ))}
         </div>
--- src/components/roadmaps/questions/[uid]/question-card.tsx
+++ src/components/roadmaps/questions/[uid]/question-card.tsx
@@ -3,46 +3,76 @@ import { Button } from '@/components/ui/button';
 import { Grid } from '@/components/ui/grid';
 import { RoadmapUserQuestions } from '@/types/Roadmap';
 import { capitalise, getQuestionDifficultyColor } from '@/utils';
-import { ArrowUpRight } from 'lucide-react';
+import { cn } from '@/utils/cn';
+import { ArrowUpRight, Check, X } from 'lucide-react';
 import Link from 'next/link';
 
 export default function RoadmapQuestionCard(opts: {
   question: RoadmapUserQuestions;
   roadmapUid: string;
+  index: number;
 }) {
-  const { question, roadmapUid } = opts;
+  const { question, roadmapUid, index } = opts;
 
   return (
-    <Link
-      href={`/roadmap/${roadmapUid}/${question.uid}`}
-      key={question.uid}
-      className="space-y-5 items-start bg-black-75 border border-black-50 p-5 rounded-lg group w-full h-auto flex flex-col relative overflow-hidden"
-    >
-      <div className="flex flex-col w-full">
-        <div className="flex w-full justify-between">
-          <h6 className="text-base text-wrap text-start">
-            {question.question}
-          </h6>
-          <Button variant="accent" className="size-10" padding="none">
-            <ArrowUpRight className="size-5 group-hover:rotate-45 duration-300" />
-          </Button>
+    <div className="relative">
+      <div
+        className={cn(
+          'absolute top-1/2 -translate-y-1/2 -left-[35px] size-3 rounded-full flex items-center justify-center',
+          question.completed
+            ? question.userCorrect
+              ? 'bg-green-500'
+              : 'bg-red-500'
+            : 'bg-black-50'
+        )}
+      ></div>{' '}
+      <Link
+        href={`/roadmap/${roadmapUid}/${question.uid}`}
+        key={question.uid}
+        className="py-6 space-y-5 items-start bg-black-75 border border-black-50 p-5 rounded-lg group w-full h-auto flex flex-col relative overflow-hidden"
+      >
+        <div className="flex flex-col w-full">
+          <div className="flex w-full justify-between">
+            <h6 className="text-base text-wrap text-start">
+              {question.question}
+            </h6>
+            <Button variant="accent" className="size-10" padding="none">
+              <ArrowUpRight className="size-5 group-hover:rotate-45 duration-300" />
+            </Button>
+          </div>
         </div>
-      </div>
 
-      <div className="mt-5 w-full-justify-between items-end">
-        <div className="flex items-center gap-x-3">
-          {question?.difficulty && (
-            <Chip
-              text={capitalise(question.difficulty)}
-              color={getQuestionDifficultyColor(question.difficulty)}
-              textColor={getQuestionDifficultyColor(question.difficulty)}
-              ghost
-              small
-            />
-          )}
+        <div className="mt-5 w-full flex justify-between items-end">
+          <div className="flex items-center gap-x-3">
+            {question?.difficulty && (
+              <Chip
+                text={capitalise(question.difficulty)}
+                color={getQuestionDifficultyColor(question.difficulty)}
+                textColor={getQuestionDifficultyColor(question.difficulty)}
+                ghost
+                small
+              />
+            )}
+          </div>
+          {/** if the user has answered the question or not */}
+          <div className="flex items-center gap-x-1 font-ubuntu">
+            {/** display if the user has already answered the question or not */}
+            {question?.completed && (
+              <>
+                {question?.userCorrect ? (
+                  <Check className="size-4 text-green-500" />
+                ) : (
+                  <X className="size-4 text-destructive" />
+                )}
+                <div className="flex items-center gap-2">
+                  <p className="text-sm">Answered</p>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      <Grid size={20} position="bottom-right" />
-    </Link>
+        <Grid size={20} position="bottom-right" />
+      </Link>
+    </div>
   );
 }

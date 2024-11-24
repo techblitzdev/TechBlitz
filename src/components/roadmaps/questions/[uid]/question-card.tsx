5c103c92e1b16b1c2d805a8ecaf44cf7cc7600ac
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-24 18:04:37 +0000

    minor style change

--- src/components/roadmaps/questions/[uid]/question-card.tsx
+++ src/components/roadmaps/questions/[uid]/question-card.tsx
@@ -64,7 +64,7 @@ export default function RoadmapQuestionCard(opts: {
         className="py-6 mb-6 space-y-5 items-start bg-black-75 border border-black-50 p-5 rounded-lg group w-full h-auto flex flex-col relative overflow-hidden"
       >
         <div className="flex flex-col w-full">
-          <div className="flex w-full justify-between">
+          <div className="flex w-full justify-between gap-3">
             <h6 className="text-base text-wrap text-start">
               {question.question}
             </h6>

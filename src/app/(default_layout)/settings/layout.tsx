61ac3fb0ee57fc9cab08e4426d209fc4495d1a0f
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-24 19:45:31 +0000

    fix(settings): layout padding changes

--- src/app/(default_layout)/settings/layout.tsx
+++ src/app/(default_layout)/settings/layout.tsx
@@ -5,13 +5,13 @@ export default function ProfileLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="text-white flex flex-col gap-y-4 relative">
-      <div className="flex flex-col gap-y-2">
+      <div className="flex flex-col gap-y-2 px-6">
         <h1 className="text-xl md:text-3xl font-satoshi font-semibold">
           Settings
         </h1>
       </div>
       <Separator className="bg-black-50" />
-      <div className="bg-black-75 rounded-xl border border-black-50">
+      <div className="bg-black-75 rounded-xl border border-black-50 mx-6">
         {children}
       </div>
     </div>

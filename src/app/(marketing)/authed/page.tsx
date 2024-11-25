7dfdff590bc461d682a51e2f3080b48347bf81d3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-25 21:26:20 +0000

    more navigation progress

--- src/app/(marketing)/authed/page.tsx
+++ src/app/(marketing)/authed/page.tsx
@@ -1,6 +1,13 @@
 import MarketingNavigation from '@/components/marketing/navigation';
+import { useUserServer } from '@/hooks/useUserServer';
+import { redirect } from 'next/navigation';
 
-export default function AuthedPage() {
+export default async function AuthedPage() {
+  const user = useUserServer();
+  if (!user) {
+    return redirect('/');
+  }
+
   return (
     <div className="container mt-6 z-30">
       <MarketingNavigation />
--- src/components/marketing/navigation.tsx
+++ src/components/marketing/navigation.tsx
@@ -5,9 +5,11 @@ import { Button } from '../ui/button';
 export default function MarketingNavigation() {
   return (
     <div className="flex items-center justify-between">
-      <Logo />
+      <Link href="/">
+        <Logo />
+      </Link>
       <nav className="bg-black border border-black-50 rounded-2xl py-2 px-4">
-        <ul className="flex items-center gap-x-6 font-satoshi">
+        <ul className="flex items-center gap-x-8 font-inter text-sm">
           <li className="text-white hover:text-accent duration-300 hover:cursor-pointer">
             <Link href="/platform">Platform</Link>
           </li>
@@ -19,7 +21,14 @@ export default function MarketingNavigation() {
           </li>
         </ul>
       </nav>
-      <Button variant="secondary">Get Started</Button>
+      <div className="flex items-center gap-x-2">
+        <Button href="/login" variant="default">
+          Login
+        </Button>
+        <Button href="signup" variant="secondary">
+          Get Started
+        </Button>
+      </div>
     </div>
   );
 }

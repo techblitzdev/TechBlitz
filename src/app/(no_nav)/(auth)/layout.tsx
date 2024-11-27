53093228bb8db10bf05054cb1df9b347c781a3e2
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-11-27 11:40:24 +0000

    hotfix

--- src/app/(no_nav)/(auth)/layout.tsx
+++ src/app/(no_nav)/(auth)/layout.tsx
@@ -2,14 +2,10 @@ import Logo from '@/components/global/logo';
 import { redirect } from 'next/navigation';
 
 export default function AuthLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  if (process.env.NODE_ENV !== 'development') {
-    redirect('/');
-  }
-
   return (
     <div className="relative bg-dot-white/[0.2]">
       <div className="top-4 left-4 absolute z-30">

90b959a8798beb484411120d5531b23b5bd68ba7
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-01 18:59:41 +0000

    mobile amends

--- src/components/marketing/homepage/hero.tsx
+++ src/components/marketing/homepage/hero.tsx
@@ -11,15 +11,15 @@ import DashboardImg from '@/public/images/dashboard-img.png';
 
 export default function HomepageHero() {
   return (
-    <section className="py-32 grid grid-cols-12 gap-10 items-center">
-      <div className="flex flex-col gap-y-4 col-span-full lg:col-span-6">
+    <section className="py-16 lg:py-32 grid grid-cols-12 gap-4 lg:gap-16 items-center">
+      <div className="flex flex-col gap-y-4 col-span-full lg:col-span-6 xl:col-span-5">
         <Link
           href="/"
           className="group w-fit relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
           <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            Beta out now
+            Sign up for early access
             <ArrowUpRight className="ml-2 size-4 group-hover:rotate-45 duration-300" />
           </span>
         </Link>
@@ -33,7 +33,7 @@ export default function HomepageHero() {
           <strong>actually</strong> useful.
         </h6>
         <div className="flex flex-col gap-y-3">
-          <div className="flex flex-col lg:flex-row gap-3 md:items-end mt-3 w-full">
+          <div className="flex flex-col md:flex-row gap-3 md:items-end mt-3 w-full">
             <Input
               className="
 								bg-transparent p-2 placeholder:text-white/50 autofill:!bg-transparent border border-black-50
@@ -56,21 +56,21 @@ export default function HomepageHero() {
         </div>
       </div>
 
-      <div className="col-span-full lg:col-span-6 h-full">
+      <div className="col-span-full lg:col-span-6 xl:col-span-7 h-full">
         <div className="relative h-full">
           <Image
-            className="absolute top-16 -right-20 rounded-lg bg-[#000000] border border-black-50 scale-[1.30] object-contain px-1"
+            className="hidden lg:block lg:absolute lg:top-16 lg:-right-20 rounded-lg bg-[#000000] border border-black-50 lg:scale-[1.25] aspect-video"
             src={RoadmapDashboardImg}
             width={1600}
             height={1600}
             alt="Roadmap Dashboard"
           />
           <Image
-            className="z-30 absolute -right-56 scale-[1.30] rounded-lg bg-[#000000] object-contain border border-black-50 px-1"
+            className="z-30 lg:absolute lg:-right-56 lg:scale-[1.30] rounded-lg bg-[#000000] border border-black-50"
             src={DashboardImg}
             width={1600}
             height={1600}
-            alt="Roadmap Dashboard"
+            alt="Dashboard"
           />
         </div>
       </div>

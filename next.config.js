45fd8799eeb617aed497e2488b64d7546209f83b
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-07 02:15:31 +0000

    adds mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+import createMDX from '@next/mdx';
+
 const {
-  withHydrationOverlay,
-} = require("@builder.io/react-hydration-overlay/next");
+  withHydrationOverlay
+} = require('@builder.io/react-hydration-overlay/next');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,16 +12,24 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.gravatar.com',
         port: '',
-        pathname: '/avatar/**',
+        pathname: '/avatar/**'
       }
-    ],
+    ]
   },
-}
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+};
 
 module.exports = withHydrationOverlay({
   /**
    * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
    * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
    */
-  appRootSelector: "main",
+  appRootSelector: 'main'
 })(nextConfig);
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);

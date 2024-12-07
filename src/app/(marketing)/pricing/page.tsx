c88e19000010824a61cc8dec3b00d4a2feb26cc3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-07 22:00:24 +0000

    more copy

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -9,8 +9,11 @@ export default function PricingPage() {
   return (
     <div className="min-h-screen container py-16 md:pb-20 md:pt-32 xl:py-40">
       <h1 className="text-3xl lg:text-5xl !font-onest !font-medium !leading-[1.1] text-gradient from-white to-white/75">
-        Pricing Page
+        Prices that don't break the bank
       </h1>
+      <p className="text-gray-400">
+        Sign up for free, upgrade for our latest and premium features.
+      </p>
     </div>
   );
 }

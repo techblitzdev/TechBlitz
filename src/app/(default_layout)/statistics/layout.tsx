6e11f359a157c4f83512b40d18e41a4a9ab3a85b
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-08 17:13:47 +0000

    adds current streak to stats layout

--- src/app/(default_layout)/statistics/layout.tsx
+++ src/app/(default_layout)/statistics/layout.tsx
@@ -1,4 +1,5 @@
 import BackToDashboard from '@/components/global/back-to-dashboard';
+import CurrentStreak from '@/components/global/current-streak';
 import { Separator } from '@/components/ui/separator';
 
 export default function StatisticsLayout({
@@ -6,8 +7,9 @@ export default function StatisticsLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="text-white flex flex-col gap-y-4 relative h-full">
-      <div className="flex flex-col gap-y-2 w-full container">
+      <div className="flex w-full justify-between container">
         <BackToDashboard />
+        <CurrentStreak />
       </div>
       <Separator className="bg-black-50" />
       <div className="container">{children}</div>

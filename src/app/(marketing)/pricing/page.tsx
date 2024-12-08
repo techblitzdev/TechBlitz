90188d90f8065b8e96919a79b585d557ba637ea3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-08 00:21:54 +0000

    adds cta block to pricing page

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -1,3 +1,4 @@
+import CallToActionBlock from '@/components/marketing/global/call-to-action-block';
 import { AnimatedBreak } from '@/components/marketing/pricing/animated-break';
 import PricingCardBlock from '@/components/marketing/pricing/pricing-card-block';
 import { Metadata } from 'next';
@@ -22,8 +23,8 @@ export const metadata: Metadata = {
 
 export default function PricingPage() {
   return (
-    <div className="text-center min-h-screen container py-16 md:pb-20 md:pt-32 xl:py-40 flex flex-col gap-y-16">
-      <div className="flex flex-col gap-y-2 items-center">
+    <div className="text-center min-h-screen container flex flex-col">
+      <div className="flex flex-col gap-y-2 items-center py-16 md:pb-20 md:pt-32 xl:py-40">
         <div className="group w-fit relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
@@ -36,8 +37,11 @@ export default function PricingPage() {
         <p className="text-gray-400">
           Start for free and unlock premium features with our affordable plans.
         </p>
+        <div className="mt-10">
+          <PricingCardBlock />
+        </div>
       </div>
-      <PricingCardBlock />
+      <CallToActionBlock title="Master Coding in Weeks, Not Years" />
     </div>
   );
 }
--- src/components/marketing/pricing/pricing-card-block.tsx
+++ src/components/marketing/pricing/pricing-card-block.tsx
@@ -6,7 +6,7 @@ export default async function PricingCardBlock() {
   const products = await getStripeProducts();
 
   return (
-    <section className="max-w-5xl grid grid-cols-3 lg:grid-cols-12 gap-10 self-center">
+    <section className="pb-20 max-w-5xl grid grid-cols-3 lg:grid-cols-12 gap-10 self-center">
       {products.map((product) => (
         <PricingCard
           key={product.id}

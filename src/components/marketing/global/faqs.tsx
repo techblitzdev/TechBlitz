38963439ab6326baa6eb80b6f56dbbfeea2961d6
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-08 13:20:18 +0000

    feat(marketing): faqs block created

--- src/components/marketing/global/faqs.tsx
+++ src/components/marketing/global/faqs.tsx
@@ -6,52 +6,94 @@ import {
 } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
 import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 
 export default function FAQsBlock() {
   return (
-    <section className="pb-32 grid grid-cols-12 gap-10">
-      <div className="col-span-4 text-start flex flex-col gap-y-2">
-        <h3 className="text-xl lg:text-3xl !font-onest !leading-[normal] text-gradient from-white to-white/55">
-          A collection of frequently asked questions.
+    <section className="pb-32 flex flex-col items-center gap-10">
+      <div className="text-center flex flex-col gap-y-2">
+        <h3 className="text-2xl lg:text-4xl !font-onest !leading-[normal] text-gradient from-white to-white/55">
+          Frequently asked questions
         </h3>
-        <p className="text-gray-400"></p>
-        <Button
-          href="/faqs"
-          variant="accent"
-        >
-          View all FAQs
-          <ChevronRight className="w-4 h-4 ml-2" />
-        </Button>
       </div>
       <Accordion
         type="single"
         collapsible
-        className="col-span-8 text-start"
+        className="text-start max-w-3xl w-full"
       >
-        <AccordionItem value="What is techblitz?">
+        <AccordionItem
+          value="What is techblitz?"
+          className="border-b border-black-50"
+        >
           <AccordionTrigger className="text-lg duration-300">
             What is techblitz?
           </AccordionTrigger>
           <AccordionContent className="pb-4">
-            techblitz is a software development company that specializes in
-            creating software solutions for businesses.
+            techblitz in a online platform that helps developers of all
+            abilities to learn and grow. We offer a range of tools and resources
+            to help you become a better developer, including questions,
+            roadmaps, tutorials, and more.
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="How do I get started?">
+        <AccordionItem
+          value="How do I get started?"
+          className="border-b border-black-50"
+        >
           <AccordionTrigger className="text-lg duration-300">
-            How do I get started?
+            Is techblitz open source?
           </AccordionTrigger>
           <AccordionContent className="pb-4">
-            You can get started by signing up for an account on our website.
+            Yes! techblitz is open source. You can find our source{' '}
+            <Link
+              href="https://git.new/blitz"
+              target="_blank"
+              className="text-accent"
+            >
+              here
+            </Link>
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="What are the benefits of using techblitz?">
+        <AccordionItem
+          value="Is techblitz free to use?"
+          className="border-b border-black-50"
+        >
+          <AccordionTrigger className="text-lg duration-300">
+            Is techblitz free to use?
+          </AccordionTrigger>
+          <AccordionContent className="pb-4">
+            Yes, techblitz is free to use. You can sign up for a free account
+            and start using our software right away.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value="What are the benefits of using techblitz?"
+          className="border-b border-black-50"
+        >
           <AccordionTrigger className="text-lg duration-300">
             What are the benefits of using techblitz?
           </AccordionTrigger>
           <AccordionContent className="pb-4">
-            There are many benefits to using techblitz, including increased
-            productivity and efficiency.
+            We offer short-form questions on
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value="What will you be adding to techblitz in the future?"
+          className="border-b border-black-50"
+        >
+          <AccordionTrigger className="text-lg duration-300">
+            What will you be adding to techblitz in the future?
+          </AccordionTrigger>
+          <AccordionContent className="pb-4">
+            We are always working on new features and improvements to techblitz.
+            You can find our roadmap{' '}
+            <Link
+              href="/roadmap"
+              className="text-accent"
+            >
+              here.{' '}
+            </Link>
+            Feel free to reach out to us with any feature requests or
+            suggestions you may have.
           </AccordionContent>
         </AccordionItem>
       </Accordion>

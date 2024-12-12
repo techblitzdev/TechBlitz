1575f422a7ebcdb4d649044f99b3a6e5b21a2f45
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-12 20:18:51 +0000

    fix: user feedback

--- src/components/questions/question-card.tsx
+++ src/components/questions/question-card.tsx
@@ -1,9 +1,8 @@
 import { QuestionWithoutAnswers } from '@/types/Questions';
 import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
-import Chip from '../global/chip';
+import Chip from '@/components/global/chip';
 import { capitalise, getQuestionDifficultyColor } from '@/utils';
-import { Grid } from '../ui/grid';
 import TagDisplay from './previous/tag-display';
 import { getQuestionStats } from '@/actions/questions/get-question-stats';
 import Link from 'next/link';
@@ -17,85 +16,67 @@ export default async function QuestionCard(opts: {
   const questionStats = await getQuestionStats(questionData.uid);
 
   return (
-    <>
-      <Link
-        href={`/question/${questionData.uid}`}
-        key={questionData.uid}
-        className="space-y-5 items-start border border-black-50 p-5 rounded-lg group w-full h-auto flex flex-col relative overflow-hidden"
-      >
-        <div className="flex flex-col w-full">
-          <div className="flex w-full justify-between">
-            <h6 className="text-base text-wrap text-start">
-              {questionData.question}
-            </h6>
-            <Button
-              variant="accent"
-              className="size-10"
-              padding="none"
-            >
-              <ArrowUpRight className="size-5 group-hover:rotate-45 duration-300" />
-            </Button>
-          </div>
-          <div className="text-start text-[10px]">
-            <p className="font-ubuntu text-sm">
-              Submissions:{' '}
-              <span className="font-medium underline">
-                {questionStats?.totalSubmissions}
-              </span>
-            </p>
-            <p className="font-ubuntu text-sm">
-              Correct submissions:{' '}
-              <span className="font-medium underline">
-                {questionStats?.percentageCorrect}%
-              </span>
-            </p>
-          </div>
+    <Link
+      href={`/question/${questionData.uid}`}
+      key={questionData.uid}
+      className="space-y-5 items-start border border-black-50 p-5 rounded-lg group w-full h-auto flex flex-col relative overflow-hidden"
+    >
+      <div className="flex flex-col gap-y-2 w-full">
+        <div className="flex w-full justify-between">
+          <h6 className="text-base text-wrap text-start">
+            {questionData?.question?.length > 100
+              ? `${questionData.question.slice(0, 100)}...`
+              : questionData?.question}
+          </h6>
+          <Button
+            variant="accent"
+            className="size-10"
+            padding="none"
+          >
+            <ArrowUpRight className="size-5 group-hover:rotate-45 duration-300" />
+          </Button>
         </div>
-        <div className="mt-5 w-full flex justify-between items-end z-10 relative">
-          <div className="flex gap-4 items-end">
-            {questionData?.tags?.length
-              ? questionData?.tags?.length > 0 && (
-                  <div className="space-y-0.5 text-start">
-                    <div className="flex items-center gap-1">
-                      <TagDisplay tags={questionData?.tags || []} />
-                    </div>
+        <div className="text-start text-[10px]">
+          <p className="font-ubuntu text-sm">
+            Submissions:{' '}
+            <span className="font-medium underline">
+              {questionStats?.totalSubmissions}
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="mt-5 w-full flex justify-between items-end z-10 relative">
+        <div className="flex gap-4 items-end">
+          {questionData?.tags?.length
+            ? questionData?.tags?.length > 0 && (
+                <div className="space-y-0.5 text-start">
+                  <div className="flex items-center gap-1">
+                    <TagDisplay tags={questionData?.tags || []} />
                   </div>
-                )
-              : ''}
-          </div>
-          <div className="flex items-center gap-x-3">
-            {questionData?.difficulty && (
-              <Chip
-                text={capitalise(questionData.difficulty)}
-                color={getQuestionDifficultyColor(questionData.difficulty)}
-                textColor={getQuestionDifficultyColor(questionData.difficulty)}
-                ghost
-                small
-              />
-            )}
-            {/** question date */}
-            {questionData?.questionDate && (
-              <Chip
-                color="black-100"
-                text={questionData.questionDate}
-                border="black-50"
-              />
-            )}
-          </div>
+                </div>
+              )
+            : ''}
         </div>
-        <Grid
-          size={20}
-          position="bottom-right"
-        />
-      </Link>
-      {/* {showAnswerModal && (
-        <PreviousQuestionAnswerModal
-          isOpen={showAnswerModal !== undefined}
-          onClose={() => setShowAnswerModal(undefined)}
-          userAnswer={showAnswerModal}
-          questionData={questionData}
-        />
-      )} */}
-    </>
+        <div className="flex items-center gap-x-3">
+          {questionData?.difficulty && (
+            <Chip
+              text={capitalise(questionData.difficulty)}
+              color={getQuestionDifficultyColor(questionData.difficulty)}
+              textColor={getQuestionDifficultyColor(questionData.difficulty)}
+              ghost
+              small
+            />
+          )}
+          {/** question date */}
+          {questionData?.questionDate && questionData?.dailyQuestion && (
+            <Chip
+              color="black-100"
+              text={questionData.questionDate}
+              border="black-50"
+            />
+          )}
+        </div>
+      </div>
+    </Link>
   );
 }

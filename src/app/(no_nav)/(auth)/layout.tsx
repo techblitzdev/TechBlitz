c3fb2d240e7969350e984cdaef9b03400c4272a5
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-12 22:17:22 +0000

    adds resend

--- src/app/(no_nav)/(auth)/layout.tsx
+++ src/app/(no_nav)/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/components/ui/logo';
 import { StarsBackground } from '@/components/ui/stars-background';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default function AuthLayout({
@@ -10,9 +11,12 @@ export default function AuthLayout({
   return (
     <div className="relative container">
       <StarsBackground className="-z-10" />
-      <div className="absolute top-8 left-0 lg:left-8">
+      <Link
+        href="/"
+        className="absolute top-8 left-0 lg:left-8"
+      >
         <Logo />
-      </div>
+      </Link>
       <div className="container text-white h-screen flex items-center justify-center">
         {children}
       </div>

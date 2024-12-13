f22a7c004ea6eb3c21effe9d04f726e0fb17b238
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-13 22:08:42 +0000

    feat: waitlist signup confirmation

--- src/actions/waitlist/add.ts
+++ src/actions/waitlist/add.ts
@@ -1,14 +1,36 @@
 'use server';
+import { resend } from '@/lib/resend';
 import { prisma } from '@/utils/prisma';
+import WaitlistConfirmationEmail from '@/components/templates/waitlist';
+import { renderAsync } from '@react-email/components';
+import React from 'react';
 
 export const addToWaitlist = async (email: string) => {
   if (!email || email.length === 0) {
     throw new Error('Email is required');
   }
 
-  return await prisma.waitlist.create({
+  const user = await prisma.waitlist.create({
     data: {
       email
     }
+  });
+
+  if (!user) {
+    throw new Error('Failed to add user to waitlist');
+  }
+
+  const html = await renderAsync(
+    React.createElement(WaitlistConfirmationEmail, {
+      email
+    })
+  );
+
+  // send the user an email
+  await resend.emails.send({
+    from: 'welcome <team@techblitz.dev>',
+    to: [email],
+    subject: 'Waitlist Confirmation',
+    html
   });
 };

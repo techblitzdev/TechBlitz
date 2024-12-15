7fa18aaeab93f7a8b47e6dfaf87563e250fc113d
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-15 14:58:57 +0000

    marketing: metadata changes

--- src/app/(marketing)/daily-challenge/layout.tsx
+++ src/app/(marketing)/daily-challenge/layout.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'techblitz | Daily Challenge | JavaScript',
+  description: 'Daily coding challenge to improve your coding skills.',
+  keywords: [
+    'coding',
+    'learn to code',
+    'programming',
+    'daily challenge',
+    'coding challenge',
+    'web development',
+    'tech skills assessment'
+  ],
+  robots: {
+    index: true,
+    follow: true
+  }
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className="flex flex-col h-full">{children}</div>;
 }

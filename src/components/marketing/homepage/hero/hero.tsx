44dabd5c899c9eb14fe47eb97b199e964d786be5
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-15 17:54:28 +0000

    homepage hero copy change

--- src/components/marketing/homepage/hero/hero.tsx
+++ src/components/marketing/homepage/hero/hero.tsx
@@ -14,9 +14,8 @@ export default function HomepageHero() {
       <div className="flex flex-col gap-y-4 col-span-full items-center text-center">
         <div className="group w-fit relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            Sign up for early access
-            <ArrowUpRight className="ml-2 size-4 group-hover:rotate-45 duration-300" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 font-medium text-white backdrop-blur-3xl">
+            Sign up to get 50% off your first month
           </span>
         </div>
         <h1 className="text-5xl lg:text-7xl !font-onest !font-medium tracking-tight">

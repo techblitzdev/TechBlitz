393e297aae4ec796c747cf5680a292cddbb8fb32
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-16 01:13:30 +0000

    feat: adding some blocks to features/roadmap page

--- src/app/(marketing)/features/roadmaps/page.tsx
+++ src/app/(marketing)/features/roadmaps/page.tsx
@@ -1,5 +1,6 @@
 import FeatureRoadmapHeroBlock from '@/components/marketing/features/roadmap/roadmap-hero';
 import CallToActionBlock from '@/components/marketing/global/call-to-action-block';
+import FAQsBlock from '@/components/marketing/global/faqs';
 import HomepageLargeText from '@/components/marketing/large-text';
 import { Metadata } from 'next';
 
@@ -9,11 +10,86 @@ export const metadata: Metadata = {
     'Create your own progression path with our AI powered roadmaps, designed to help you grow as a developer.'
 };
 
+const faqs = [
+  {
+    question: 'When will TechBlitz launch?',
+    answer:
+      'TechBlitz is set to launch in Q1 2025! Join our waitlist today to get early access and be notified as soon as we go live.'
+  },
+  {
+    question: "How can I access the roadmaps on TechBlitz's platform?",
+    answer: (
+      <>
+        To access the roadmaps on techblitz, you need to have a premium account.
+        You can sign up for a premium account{' '}
+        <a
+          href="/pricing"
+          className="!text-accent underline"
+        >
+          here
+        </a>
+        .
+      </>
+    )
+  },
+  {
+    question: 'What is TechBlitz, and how can it help developers?',
+    answer:
+      'TechBlitz is an innovative online learning platform designed for developers of all skill levels. Our tools, including quizzes, coding roadmaps, and tutorials, help you sharpen your skills, boost productivity, and stay ahead in the tech industry.'
+  },
+  {
+    question: 'Is TechBlitz open source?',
+    answer: (
+      <>
+        Yes, TechBlitz is completely open source! Explore our source code on{' '}
+        <a
+          href="https://github.com/logannford/TechBlitz/"
+          target="_blank"
+          className="!text-accent underline"
+        >
+          GitHub
+        </a>{' '}
+        and join the growing community of developers contributing to our
+        platform.
+      </>
+    )
+  },
+  {
+    question: 'Is TechBlitz free to use?',
+    answer:
+      'Absolutely! TechBlitz offers a free plan to get you started right away. Create an account and dive into our rich library of developer resources today.'
+  },
+  {
+    question: 'What are the key benefits of using TechBlitz?',
+    answer:
+      'TechBlitz provides engaging, short-form coding questions and practical roadmaps to help developers enhance their skills and tackle real-world challenges. Learn faster, smarter, and with less overwhelm!'
+  },
+  {
+    question: 'What will you be adding to techblitz in the future?',
+    answer: (
+      <>
+        We’re constantly improving TechBlitz with new features and updates.
+        Check out our{' '}
+        <a
+          href="https://github.com/users/Logannford/projects/5"
+          target="_blank"
+          className="text-accent"
+        >
+          roadmap
+        </a>{' '}
+        to see what’s next, and share your suggestions — we’d love to hear your
+        ideas!
+      </>
+    )
+  }
+];
+
 export default function FeatureDailyQuestionPage() {
   return (
     <div className="container">
       <FeatureRoadmapHeroBlock />
 
+      <FAQsBlock faqs={faqs} />
       <CallToActionBlock
         title="The smarter way to stay on top of tech"
         description="Create your own progression path with our AI powered roadmaps, designed to help you grow as a developer."
--- src/components/marketing/global/faqs.tsx
+++ src/components/marketing/global/faqs.tsx
@@ -15,10 +15,16 @@ export default function FAQsBlock(opts: { faqs: FAQ[] }) {
 
   return (
     <section className="pb-32 flex flex-col items-center gap-10">
-      <div className="text-center flex flex-col gap-y-2">
-        <h2 className="text-2xl lg:text-4xl !font-onest !leading-[normal] text-gradient from-white to-white/55">
+      <div className="text-center flex flex-col gap-y-2 items-center">
+        <div className="group w-fit relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Questions & Answers
+          </span>
+        </div>
+        <h1 className="text-2xl lg:text-4xl !font-onest !leading-[normal] text-gradient from-white to-white/55">
           Frequently asked questions
-        </h2>
+        </h1>
       </div>
       <Accordion
         type="single"

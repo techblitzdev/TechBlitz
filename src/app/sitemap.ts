72844faa4f7058440cf0a6572c45ecb239dfec23
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-17 01:57:29 +0000

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,38 @@
 import type { MetadataRoute } from 'next';
 
+const baseUrl = 'https://techblitz.dev';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://techblitz.dev',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1
+    },
+    {
+      url: `${baseUrl}/features/roadmaps`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9
+    },
+    {
+      url: `${baseUrl}/pricing`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6
+    },
+    {
+      url: `${baseUrl}/faqs`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5
+    },
+    {
+      url: `${baseUrl}/open-source`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5
     }
   ];
 }

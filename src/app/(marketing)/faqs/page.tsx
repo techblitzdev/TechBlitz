47cad0e661c0112b4279528d972c9d874db42bfe
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-17 01:18:19 +0000

    faq heading hotfix

--- src/app/(marketing)/faqs/page.tsx
+++ src/app/(marketing)/faqs/page.tsx
@@ -224,7 +224,7 @@ export default function FAQsPage() {
         <div className="mt-28 z-10 absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-[#000] to-transparent pointer-events-none"></div>
 
         <div className="flex flex-col gap-y-3 items-center z-20 relative">
-          <h1 className="text-5xl lg:text-7xl !font-onest !font-medium tracking-tight text-gradient from-white to-white/75 text-center PY-1">
+          <h1 className="text-5xl lg:text-7xl !font-onest !font-medium tracking-tight text-gradient from-white to-white/75 text-center py-1">
             Frequently Asked Questions
           </h1>
           <p className="text-gray-400 max-w-xl text-center text-sm md:text-base font-onest">

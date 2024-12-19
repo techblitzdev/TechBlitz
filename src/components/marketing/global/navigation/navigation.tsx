8401e98a8e117d4ffd1efe50053695e593b94393
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-19 23:29:17 +0000

    fix(marketing): mobile menu changes (responsive issues)

--- src/components/marketing/global/navigation/navigation.tsx
+++ src/components/marketing/global/navigation/navigation.tsx
@@ -6,7 +6,7 @@ import { MobileMenu } from './mobile-menu';
 
 import dynamic from 'next/dynamic';
 const NavigationButtons = dynamic(() => import('./navigation-buttons'), {
-  ssr: false
+  ssr: false,
 });
 
 export default function MarketingNavigation() {
@@ -14,23 +14,17 @@ export default function MarketingNavigation() {
     <div className="fixed w-full py-5 z-[1000] bg-[#000000]">
       <div className="container flex items-center justify-between w-full">
         {/* Logo */}
-        <Link
-          href="/"
-          aria-label="Go back to dashboard"
-        >
+        <Link href="/" aria-label="Go back to dashboard">
           <Logo />
         </Link>
-
         {/* Navigation Menu */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 py-2 px-4 hidden md:block">
+        <div className="lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 py-2 px-4 hidden lg:block">
           <NavigationMenuItems />
         </div>
-
-        <div className="block md:hidden">
+        <div className="block lg:hidden">
           {/* Mobile Menu */}
           <MobileMenu />
         </div>
-
         <NavigationButtons />
       </div>
     </div>

97d402140ef3a424149be0c420ef7f8d5cb45e52
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-21 03:12:43 +0000

    fix: minor style changes

--- src/components/marketing/features/roadmap/roadmap-hero.tsx
+++ src/components/marketing/features/roadmap/roadmap-hero.tsx
@@ -8,9 +8,9 @@ export default function FeatureRoadmapHeroBlock() {
     <section className="pt-32 lg:pt-52 pb-36 relative">
       <div className="mt-28 z-10 absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-[#000] to-transparent pointer-events-none"></div>
 
-      <div className="flex flex-col gap-y-3 items-center z-20 relative">
+      <div className="flex flex-col gap-y-6 items-center z-20 relative">
         <h1 className="text-5xl lg:text-7xl !font-onest !font-medium tracking-tight text-gradient from-white to-white/75 text-center py-1.5">
-          Code your way.
+          Code your <br /> way.
         </h1>
         <p className="text-gray-400 max-w-xl text-center text-sm md:text-base font-onest">
           Unlock your potential as a software engineer with personalized

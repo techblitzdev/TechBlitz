fef7a9417745b4aa9e3f9bc0f8351f0abe1d741b
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-21 02:32:52 +0000

    fix(marketing): daily-challenge-hero first ui created

--- src/components/marketing/features/daily-challenge/hero/daily-challenge-hero.tsx
+++ src/components/marketing/features/daily-challenge/hero/daily-challenge-hero.tsx
@@ -4,17 +4,19 @@ import FeatureDailyChallengeHeroGraphic from './graphic';
 
 export default function FeatureDailyChallengeHero() {
   return (
-    <section className="relative flex gap-10 text-white overflow-hidden">
-      <div className="flex flex-col gap-y-6 w-full xl:w-2/5 pt-32 lg:pt-52 pb-36 z-10">
+    <section className="relative flex gap-10 text-white overflow-hidden justify-center">
+      <div className="flex flex-col gap-y-6 w-full xl:w-2/5 pt-32 lg:pt-52 pb-36 z-10 items-center">
         <AnimatedSpan content="Daily Coding Challenges" />
         <h1
-          className="text-5xl lg:text-6xl !font-onest font-bold tracking-tight py-1.5
-          text-transparent bg-clip-text bg-gradient-to-r from-white to-white/75 leading-tight"
+          className="
+            text-5xl lg:text-6xl !font-onest font-bold tracking-tight py-1.5 text-center
+            text-transparent bg-clip-text bg-gradient-to-r from-white to-white/75 leading-tight
+          "
         >
-          Coding costs time and money, we eliminate both.
+          Learn to code faster.
         </h1>
 
-        <p className="text-white/70 max-w-xl text-base font-onest">
+        <p className="text-white/70 max-w-xl text-base font-onest text-center">
           Code challenges do not need to be time consuming or boring. With
           techblitz, you can learn something new every day. In 5 minutes, from
           any device.
@@ -29,19 +31,11 @@ export default function FeatureDailyChallengeHero() {
             >
               Answer today's challenge
             </Button>
-            <p className="text-xs text-gray-400">(No sign up required)</p>
           </div>
-          <Button
-            variant="accent"
-            className="flex items-center gap-2"
-          >
+          <Button variant="accent" className="flex items-center gap-2">
             Sign up
           </Button>
         </div>
-      </div>
-
-      <div className="w-3/5 relative hidden lg:block">
-        <FeatureDailyChallengeHeroGraphic />
       </div>
     </section>
   );

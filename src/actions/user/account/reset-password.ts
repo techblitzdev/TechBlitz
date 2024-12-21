df70e8b800dfcb3f3f8b03355436d989bfff68e2
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-21 12:35:32 +0000

    chore: auth email name change

--- src/actions/user/account/reset-password.ts
+++ src/actions/user/account/reset-password.ts
@@ -6,9 +6,8 @@ const redirectTo = process.env.NEXT_PUBLIC_PASSWORD_RESET_REDIRECT_URL;
 export const resetPassword = async (opts: { email: string }) => {
   const { email } = opts;
 
-  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo,
-  });
+  // we handle the redirect in the edge function
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email);
 
   if (error) {
     throw error;

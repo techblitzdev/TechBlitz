578b1000f5980e02eaedbdb238dad7ca75495ea7
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-24 16:17:15 +0000

    hotfix(stats): mobile view stats width change

--- src/components/statistics/suggested-questions.tsx
+++ src/components/statistics/suggested-questions.tsx
@@ -12,7 +12,7 @@ export default async function SuggestedQuestions() {
   const suggestions = await getSuggestions({ userUid: user.uid });
 
   return (
-    <section className="col-span-full md:col-span-6 border border-black-50 rounded-lg flex flex-col divide-y-[1px] divide-black-50 overflow-hidden">
+    <section className="col-span-full lg:col-span-6 border border-black-50 rounded-lg flex flex-col divide-y-[1px] divide-black-50 overflow-hidden">
       <div className="flex flex-col gap-2.5 px-3 py-4">
         <h2 className="text-2xl font-onest">Suggested Questions</h2>
         <p className="text-xs text-gray-400">

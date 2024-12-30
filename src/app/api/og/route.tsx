72896505895ac98682f79474874cfb9bf6ecdb26
Logan Ford <110533855+Logannford@users.noreply.github.com>
2024-12-30 20:57:30 +0000

    chore: more playing around with the og image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,37 +1,62 @@
 import { ImageResponse } from 'next/og';
+import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
-  // Get text from query parameter or use default
+  // fetch the Ubuntu font from Google Fonts
+  const ubuntuFont = await fetch(
+    new URL(
+      'https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap',
+      request.url
+    )
+  ).then((res) => res.arrayBuffer());
+
+  // get text from query parameter or use default
   const text = searchParams.get('text') || 'Hello, World!';
 
-  // Get background color from query parameter or use default
+  // get background color from query parameter or use default
   const bgColor = searchParams.get('bgColor') || '#000000';
 
-  // Get text color from query parameter or use default
+  // get text color from query parameter or use default
   const textColor = searchParams.get('textColor') || '#ffffff';
 
+  // generate and return the OG image
   return new ImageResponse(
     (
       <div
-        tw={`flex items-center justify-center w-full h-full`}
         style={{
-          background: bgColor,
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: bgColor,
+          fontSize: 32,
+          fontWeight: 600,
+          fontFamily: 'Ubuntu',
         }}
       >
-        <div tw="flex flex-col items-center justify-center">
-          <h1 tw={`text-6xl font-bold`} style={{ color: textColor }}>
-            {text}
-          </h1>
-        </div>
+        <div style={{ marginTop: 40, color: textColor }}>{text}</div>
+        <p style={{ color: textColor, fontSize: '14px', fontWeight: '400' }}>
+          The open-source, mobile-friendly software education platform
+        </p>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: 'Ubuntu',
+          data: ubuntuFont,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
     }
   );
 }

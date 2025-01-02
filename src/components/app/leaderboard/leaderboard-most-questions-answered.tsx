cc21ecd083b9204803923a2f68e9bf3cb699f98b
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-02 22:28:07 +0000

    fix(leaderboard): fixes leaderboard spacing & alignment issues

--- src/components/app/leaderboard/leaderboard-most-questions-answered.tsx
+++ src/components/app/leaderboard/leaderboard-most-questions-answered.tsx
@@ -1,6 +1,9 @@
 import { getMostQuestionsAnswered } from '@/actions/leaderboard/get-most-questions-answered';
 import Card from '@/components/global/Card';
 import ProfilePicture from '@/components/ui/profile-picture';
+import { UserRecord } from '@/types/User';
+import { shortenText } from '@/utils';
+import { getUserDisplayName } from '@/utils/user';
 import { Trophy } from 'lucide-react';
 
 const header = () => {
@@ -23,43 +26,47 @@ export default async function LeaderboardMostQuestionsAnswered(opts: {
   userUid?: string;
 }) {
   const { userUid } = opts;
-
   const topUsersByQuestionCount = await getMostQuestionsAnswered();
 
   return (
     <Card header={header()}>
       <div className="flex flex-col divide-y-[1px] divide-black-50">
         {/* Headings Row */}
-        <div className="flex items-center justify-between px-4 py-2 bg-black-75 font-medium font-ubuntu text-xs">
-          <span className="flex-1">Position</span>
-          <span className="flex-1">User</span>
-          <span className="flex-1 text-right">Answered</span>
+        <div className="flex items-center px-4 py-2 bg-black-75 font-medium font-ubuntu text-xs">
+          <span className="w-[30%]">Position</span>
+          <span className="w-[50%]">User</span>
+          <span className="w-[20%] text-right">Answered</span>
         </div>
 
         {topUsersByQuestionCount.map((user, index) => (
           <div
             key={user.uid}
-            className={`flex items-center justify-between px-4 py-3 ${
+            className={`flex items-center px-4 py-3 ${
               index % 2 === 0 ? 'bg-black' : 'bg-[#000]'
             }`}
           >
             {/* Position */}
-            <span className="flex-1">#{index + 1}</span>
+            <span className="w-[30%]">#{index + 1}</span>
 
             {/* User */}
-            <div className="flex flex-1 items-center gap-4">
+            <div className="w-[50%] flex items-center gap-4">
               <ProfilePicture
                 src={user.userProfilePicture}
-                alt={user.username}
+                alt={`${user.username} profile picture`}
               />
-              <span>{user.username || 'Anonymous'}</span>
+              <span>
+                {shortenText(
+                  getUserDisplayName(user as unknown as UserRecord),
+                  25
+                )}
+              </span>
               {userUid === user.uid && (
                 <span className="text-xs text-gray-500">(You)</span>
               )}
             </div>
 
             {/* Answered */}
-            <span className="w-[35%] text-right">{user._count.answers}</span>
+            <span className="w-[20%] text-right">{user._count.answers}</span>
           </div>
         ))}
       </div>

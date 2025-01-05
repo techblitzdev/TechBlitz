abb0049c6d42a6eea1012504032f11bff3e233eb
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-05 21:48:49 +0000

    hotfix: removes send live email btn in admin area

--- src/app/(app)/admin/page.tsx
+++ src/app/(app)/admin/page.tsx
@@ -1,13 +1,10 @@
 'use client';
 import NewQuestionModal from '@/components/app/questions/new-question-modal';
-import { Button } from '@/components/ui/button';
-import { sendLiveEmail } from '@/actions/waitlist/send-live-email';
 
 export default function AdminPage() {
   return (
     <div className="px-6 h-screen flex items-center justify-center">
       <NewQuestionModal className="w-fit" />
-      <Button onClick={() => sendLiveEmail()}>Send Live Email</Button>
     </div>
   );
 }

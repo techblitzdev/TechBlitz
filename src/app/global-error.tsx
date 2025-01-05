53d01964e327e0428bd3da62f78338e3d4480fad
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-05 16:18:51 +0000

    chore: remove sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import * as Sentry from '@sentry/nextjs';
 import Error from 'next/error';
-import { useEffect } from 'react';
 import NextError from 'next/error';
 
 export default function GlobalError({ error }: { error: Error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
   return (
     <html>
       <body>

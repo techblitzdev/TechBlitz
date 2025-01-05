64982e83a3be96d76c35d6265003650585df4a7a
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-05 16:58:43 +0000

    chore: removing un used files

--- src/app/(app)/(default_layout)/(roadmaps)/roadmaps/page.client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function ClientPage({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
--- src/utils/constants/daily-streak.ts
+++
@@ -1,1 +0,0 @@
-export const DAILY_STREAK = 'Nice work!';

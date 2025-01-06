87b5b199b600cb05b202fa7e633420dd69af3f43
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-06 22:22:14 +0000

    hotfix(auth): more auth onboarding changes to confirm url

--- src/app/api/auth/confirm/route.ts
+++ src/app/api/auth/confirm/route.ts
@@ -6,71 +6,58 @@ import { prisma } from '@/utils/prisma';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  // pull the token has out of the query params
-  // this gets set on the OTP in the email sent to the user
-  // on signup
-  const token_hash = searchParams.get('token_hash');
-  // this will be the type of OTP that was sent to the user
-  const type = searchParams.get('type') as EmailOtpType | null;
 
-  // get the next URL from the query params - default to login for now
+  // Extract parameters from the query string
+  const token_hash = searchParams.get('token_hash');
+  const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/login';
 
-  // clone the url so we can modify it
-  const redirectTo = request.nextUrl.clone();
-  // set the pathname to the next URL that is set above
+  // Prepare the redirect URL
+  const redirectTo = new URL(request.nextUrl.origin);
   redirectTo.pathname = next;
 
-  // delete the query params from the URL
-  // so that the user doesn't see them when they get redirected
-  // as javascript objects (the url object) are passed by reference
-  // and not value. This means changing the searchParams on the
-  // redirectTo object will also change the searchParams on the
-  // request.nextUrl object.
+  // Remove sensitive parameters from the redirect URL
   redirectTo.searchParams.delete('token_hash');
   redirectTo.searchParams.delete('type');
 
-  // if we have the required vars to verify the OTP
   if (token_hash && type) {
-    // create a new supabase client from our util
     const supabase = await createClient();
 
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
+    // Verify the OTP
+    const { error } = await supabase.auth.verifyOtp({ type, token_hash });
     if (!error) {
-      // get the current user from the supabase client
       const { data: userSession } = await supabase.auth.getUser();
 
-      // check if we have the user in the db and if not, create them
-      const existingUser = await prisma.users.findUnique({
-        where: { uid: userSession.user?.id },
-      });
+      if (userSession?.user) {
+        const { id, email } = userSession.user;
 
-      if (!existingUser) {
-        await prisma.users.create({
-          data: {
-            uid: userSession.user?.id ?? '',
-            email: userSession.user?.email ?? '',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
+        // Ensure the user exists in the database
+        const existingUser = await prisma.users.findUnique({
+          where: { uid: id },
         });
+        if (!existingUser) {
+          await prisma.users.create({
+            data: {
+              uid: id,
+              email: email ?? '',
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            },
+          });
+        }
+
+        // Add the onboarding query parameter to the URL
+        redirectTo.searchParams.set('onboarding', 'true');
+
+        // Redirect to the next page
+        return NextResponse.redirect(redirectTo.toString());
       }
-
-      // redirect the user to the next page
-      redirectTo.searchParams.delete('next');
-      // Add onboarding query param
-      redirectTo.searchParams.set('onboarding', 'true');
-
-      return NextResponse.redirect(redirectTo);
     } else {
       console.error('Error verifying OTP:', error.message);
     }
   }
 
-  // return the user to an error page with some instructions
+  // Redirect to the error page if verification fails
   redirectTo.pathname = '/error';
-  return NextResponse.redirect(redirectTo);
+  return NextResponse.redirect(redirectTo.toString());
 }

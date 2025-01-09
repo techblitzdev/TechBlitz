e4d1aa3378940125fb6c2d4f64cde5480882ff06
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-09 12:05:35 +0000

    chore: robots & sitemap changes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,17 @@
 import type { MetadataRoute } from 'next';
+import { headers } from 'next/headers';
 
 export default function robots(): MetadataRoute.Robots {
+  const headersList = headers();
+  const domain = headersList.get('host');
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/dashboard'
-    },
-    sitemap: 'https://techblitz.dev/sitemap.xml'
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: `https://${domain}/sitemap.xml`,
   };
 }

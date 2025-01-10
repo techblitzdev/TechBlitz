8e33914f598bd2b1b17607f93906507d2bebb7d7
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-10 15:42:33 +0000

    slug hotfix

--- src/components/app/questions/single/question-card.tsx
+++ src/components/app/questions/single/question-card.tsx
@@ -82,7 +82,7 @@ export default function QuestionCard(opts: {
         questionTags={'tags' in question ? question.tags : []}
         answerFormRef={answerFormRef}
         user={user}
-        redirectUrl={`/question/${question.uid}`}
+        redirectUrl={`/question/${question.slug}`}
       />
     </div>
   );

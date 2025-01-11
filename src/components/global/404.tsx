5029e3fc79a140167a3b6f8d277264d1c43474ad
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-11 15:59:22 +0000

    hotfix: 404 page hotfix

---
+++ src/components/global/404.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import CountUp from 'react-countup';
+
+export default function ErrorPageCountUp() {
+  return (
+    <CountUp
+      end={404}
+      className="min-h-24 text-8xl font-semibold font-onest"
+      duration={2}
+    />
+  );
+}

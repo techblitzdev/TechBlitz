4f1d33439b5dc2d1ed7638c8f22b2f5e635b7513
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-11 22:04:55 +0000

    fix(custom-questions): custom questions hotfixes

--- src/app/(app)/(questions)/question/custom/[uid]/layout.tsx
+++ src/app/(app)/(questions)/question/custom/[uid]/layout.tsx
@@ -4,38 +4,71 @@ import CurrentStreak from '@/components/ui/current-streak';
 import { Separator } from '@/components/ui/separator';
 import FeedbackButton from '@/components/ui/feedback-button';
 import SidebarLayoutTrigger from '@/components/global/navigation/sidebar-layout-trigger';
+import RandomQuestion from '@/components/global/random-question';
+import { createMetadata } from '@/utils/seo';
+import { capitalise } from '@/utils';
 
 // Actions
 import { getQuestion } from '@/utils/data/questions/get';
+import { QuestionSingleContextProvider } from '@/components/app/questions/single/layout/question-single-context';
+import { getUser } from '@/actions/user/authed/get-user';
+import { redirect } from 'next/navigation';
+import QuestionActionButtons from '@/components/app/questions/single/layout/question-action-buttons';
 
-export default async function Layout({
+export async function generateMetadata({
+  params,
+}: {
+  params: { uid: string };
+}) {
+  const question = await getQuestion('uid', params.uid);
+  // get the title via slug and removing the - from the slug
+  const title = question?.slug?.replace(/-/g, ' ') || 'Coding Question';
+
+  return createMetadata({
+    title: `${capitalise(title)} | TechBlitz`,
+    description: 'Boost your coding skills for free with TechBlitz',
+    image: {
+      text: `${title} | TechBlitz`,
+      bgColor: '#000000',
+      textColor: '#ffffff',
+    },
+    canonicalUrl: `/question/custom/${params.uid}`,
+  });
+}
+
+export default async function QuestionUidLayout({
   children,
   params,
-}: Readonly<{ children: React.ReactNode; params: { slug: string } }>) {
-  const { slug } = params;
+}: Readonly<{ children: React.ReactNode; params: { uid: string } }>) {
+  const { uid } = params;
 
-  const question = await getQuestion('slug', slug);
+  const question = await getQuestion('uid', uid);
+  if (!question) {
+    return redirect('/questions/custom');
+  }
+  const user = await getUser();
 
   return (
     <>
-      <div className="flex items-center justify-between py-2 px-6">
-        <div className="flex items-center gap-x-5 py-2">
-          <SidebarLayoutTrigger />
-          {/** Previous question button */}
-          <BackToDashboard href="/questions/custom" />
-          {question?.dailyQuestion && question?.questionDate && (
-            <div className="font-ubuntu flex gap-x-5 items-center">
-              <p>Daily question</p>
-            </div>
-          )}
+      <QuestionSingleContextProvider question={question} user={user}>
+        <div className="grid grid-cols-12 items-center justify-between pb-2 px-3 lg:px-6 relative">
+          <div className="col-span-2 lg:col-span-4 flex items-center gap-x-5 py-2 justify-start">
+            <SidebarLayoutTrigger />
+            {/** Previous question button */}
+            <BackToDashboard href="/questions/custom" />
+          </div>
+          <div className="col-span-7 lg:col-span-4 flex items-center justify-center">
+            <QuestionActionButtons />
+          </div>
+          <div className="col-span-3 lg:col-span-4 flex items-center gap-x-1 md:gap-x-3 justify-end">
+            <CurrentStreak />
+            <RandomQuestion identifier="uid" currentQuestionSlug={uid} />
+            <FeedbackButton reference={question?.slug || undefined} />
+          </div>
         </div>
-        <div className="flex items-center gap-x-3">
-          <CurrentStreak />
-          <FeedbackButton reference={question?.slug || undefined} />
-        </div>
-      </div>
-      <Separator className="bg-black-50" />
-      {children}
+        <Separator className="bg-black-50" />
+        {children}
+      </QuestionSingleContextProvider>
     </>
   );
 }
--- src/app/(app)/(questions)/question/custom/[uid]/page.tsx
+++ src/app/(app)/(questions)/question/custom/[uid]/page.tsx
@@ -1,15 +1,15 @@
 import { getQuestion } from '@/utils/data/questions/get';
 import { Separator } from '@/components/ui/separator';
 import NoDailyQuestion from '@/components/global/no-daily-question';
-import QuestionDisplay from '@/components/app/questions/single/layout/code-snippet';
-import { BarChartIcon as ChartColumn, Check, User } from 'lucide-react';
+import CodeDisplay from '@/components/app/questions/single/layout/code-snippet';
 import { getQuestionStats } from '@/utils/data/questions/get-question-stats';
 import { useUserServer } from '@/hooks/use-user-server';
 import QuestionCard from '@/components/app/questions/single/layout/question-card';
 import { getRandomQuestion } from '@/utils/data/questions/get-random';
 import ExpandedCodeModal from '@/components/app/questions/single/layout/expanded-code-modal';
-import RelatedQuestions from '@/components/app/questions/single/related-question-card';
 import ResizableLayout from '@/components/ui/resizable-layout';
+import AiQuestionHelp from '@/components/app/questions/single/layout/ai-question-help';
+import ChangeCodeTheme from '@/components/app/questions/single/layout/change-code-theme';
 
 export default async function TodaysQuestionPage({
   params,
@@ -20,9 +20,8 @@ export default async function TodaysQuestionPage({
 
   const user = await useUserServer();
 
-  const [question, totalSubmissions, nextQuestion] = await Promise.all([
+  const [question, nextQuestion] = await Promise.all([
     getQuestion('uid', uid),
-    getQuestionStats('uid', uid),
     getRandomQuestion({
       identifier: 'uid',
       currentQuestionSlug: uid,
@@ -34,76 +33,41 @@ export default async function TodaysQuestionPage({
   }
 
   const leftContent = (
-    <div className="flex flex-col gap-y-4 lg:pl-6 p-3">
+    <div className="flex flex-col gap-y-4 p-3 lg:p-6 lg:pr-3 h-full">
       <QuestionCard
         question={question}
         user={user}
         nextQuestion={nextQuestion}
-        identifier="uid"
+        identifier="slug"
       />
     </div>
   );
 
   const rightContent = (
-    <div className="flex flex-col gap-4 lg:pr-6 p-3">
+    <div className="hidden lg:flex flex-col gap-4 p-3 lg:p-6 lg:pl-3 h-full">
       <div
         id="code-snippet"
-        className="h-fit lg:h-[45rem] bg-black-75 border border-black-50 rounded-xl relative overflow-hidden"
+        className="bg-black-75 border border-black-50 rounded-xl relative overflow-hidden h-full"
       >
-        <div className="p-4 text-sm flex w-full items-center justify-between bg-black-25">
-          <p className="font-onest">index.js</p>{' '}
+        <div className="p-4 text-sm flex w-full items-center justify-end bg-black-25 gap-x-3">
+          {/** explain question ai button */}
+          <AiQuestionHelp question={question} user={user} />
+          {/** code theme selector */}
+          <ChangeCodeTheme user={user} />
+          {/** code snippet */}
           {question.codeSnippet && (
             <ExpandedCodeModal code={question.codeSnippet} />
           )}
         </div>
         <Separator className="bg-black-50" />
         {question?.codeSnippet && (
-          <QuestionDisplay
+          <CodeDisplay
             content={question.codeSnippet}
             backgroundColor="#111111"
+            user={user}
           />
         )}
       </div>
-
-      {!question.customQuestion && (
-        <div className="min-h-fit bg-black-75 border border-black-50 rounded-xl overflow-hidden">
-          <RelatedQuestions
-            slug={question.slug || ''}
-            tags={question.tags || []}
-          />
-        </div>
-      )}
-
-      {!question.customQuestion && (
-        <div className="bg-black-75 border border-black-50 rounded-xl overflow-hidden min-h-fit">
-          <div className="flex items-center gap-x-1 p-4 bg-black-25">
-            <ChartColumn className="size-4" />
-            <div className="text-sm">Stats</div>
-          </div>
-          <Separator className="bg-black-50" />
-          <div className="p-4 flex items-center">
-            <div className="flex items-start gap-4 text-sm text-gray-400">
-              <div className="flex items-center gap-2">
-                <div className="flex items-center gap-0.5">
-                  <User className="size-4" />
-                  <p>Total submissions:</p>
-                </div>
-                <p>{totalSubmissions?.totalSubmissions}</p>
-              </div>
-              {totalSubmissions?.percentageCorrect > 0 && (
-                <>
-                  |
-                  <div className="flex items-center gap-0.5">
-                    <Check className="size-4" />
-                    <p>Success rate:</p>
-                    <p>{totalSubmissions?.percentageCorrect}%</p>
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 

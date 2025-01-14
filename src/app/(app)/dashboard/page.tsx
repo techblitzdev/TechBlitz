68f6e72f01607bf4a85fd7af499dea0590fe44a7
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-14 18:32:43 +0000

    chore: dashboard page speed improvements

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,40 +1,34 @@
-// components
-import CurrentStreak from '@/components/ui/current-streak';
-import Feedback from '@/components/ui/feedback-button';
+import { Suspense } from 'react';
 import { Separator } from '@/components/ui/separator';
 import ClientPage from './page.client';
 import DashboardBentoGrid from '@/components/app/dashboard/dashboard-bento-grid';
-
-// utils
-import SidebarLayoutTrigger from '@/components/global/navigation/sidebar-layout-trigger';
+import DashboardHeader from '@/components/app/dashboard/dashboard-header';
+import WelcomeMessage from '@/components/app/dashboard/welcome-banner';
 import { useUserServer } from '@/hooks/use-user-server';
-import { getUserDisplayName } from '@/utils/user';
+import LoadingSpinner from '@/components/ui/loading';
 
-export default async function Dashboard({
-  searchParams,
-}: {
+interface DashboardProps {
   searchParams: { [key: string]: string | string[] | undefined };
-}) {
+}
+
+export default async function Dashboard({ searchParams }: DashboardProps) {
   const user = await useUserServer();
+  if (!user) return;
 
   return (
     <ClientPage searchParams={searchParams}>
       <div className="text-white flex flex-col gap-y-4 h-full">
-        <div className="flex w-full justify-between px-6">
-          <div className="flex items-center gap-3">
-            <SidebarLayoutTrigger />
-          </div>
-          <div className="flex items-center gap-3">
-            <CurrentStreak />
-            <Feedback />
-          </div>
-        </div>
+        <DashboardHeader />
         <Separator className="bg-black-50" />
         <div className="h-full mt-1 max-w-7xl px-6 mx-auto flex flex-col gap-5">
-          <h1 className="text-2xl font-bold">
-            Welcome back, {getUserDisplayName(user)}
-          </h1>
-          <DashboardBentoGrid />
+          <Suspense
+            fallback={<h1 className="text-2xl font-bold">Welcome back,</h1>}
+          >
+            <WelcomeMessage user={user} />
+          </Suspense>
+          <Suspense fallback={<LoadingSpinner />}>
+            <DashboardBentoGrid />
+          </Suspense>
         </div>
       </div>
     </ClientPage>

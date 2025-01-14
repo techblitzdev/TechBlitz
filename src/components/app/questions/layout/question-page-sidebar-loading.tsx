fb65532bc07db06b149d52947204ff9c1c912464
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-14 21:50:56 +0000

    feat: /questions ISR

--- src/components/app/questions/layout/question-page-sidebar-loading.tsx
+++ src/components/app/questions/layout/question-page-sidebar-loading.tsx
@@ -3,13 +3,13 @@ import { QuestionMarkCircledIcon } from '@radix-ui/react-icons';
 
 export default function QuestionPageSidebarLoading() {
   return (
-    <aside className="w-full lg:w-[45%] relative">
-      <div className="sticky top-10 space-y-10 w-1/2">
+    <aside className="w-full relative">
+      <div className="sticky top-10 space-y-10">
         <div className="w-fit h-fit flex flex-col gap-y-2.5">
           <h6 className="text-xl">Your current streak</h6>
           <Skeleton className="h-80 w-64" />
         </div>
-        <div className="space-y-4 min-w-fit">
+        <div className="space-y-4 w-full">
           <div className="flex items-center gap-x-2">
             <h6 className="text-xl">Suggested questions</h6>
             <QuestionMarkCircledIcon className="size-3.5 mt-1 text-gray-300" />

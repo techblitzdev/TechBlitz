171062649f73b17a003ed9036d9d48c1a88202e3
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-15 15:56:10 +0000

    nextjs config now applying

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withMDX = require('@next/mdx');
+const withMDX = require('@next/mdx')();
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });

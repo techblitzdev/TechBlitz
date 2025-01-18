20f235407c883e8c9dd02a1a546b35e6ba15d550
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-18 17:26:15 +0000

    feat: start of adding daily noti cron

---
+++ src/app/api/cron/send-daily-streak/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from '@/lib/prisma';
+import { resend } from '@/lib/resend';
+import { NextRequest } from 'next/server';
+
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+// my user uid for testing this.
+const TEST_USER_UID = '3a57d7e8-8b80-483b-93d0-70fe1f06b0c0';
+
+export async function GET(request: NextRequest) {
+  const user = await prisma.users.findUnique({
+    where: {
+      uid: TEST_USER_UID,
+    },
+  });
+
+  if (!user) {
+    return new Response('User not found', { status: 404 });
+  }
+
+  await resend.emails.send({
+    from: 'team@techblitz.dev',
+    to: user.email,
+    subject: 'Daily Streak',
+    html: '<p>Hello, world!</p>',
+  });
+
+  return Response.json({ success: true });
+}

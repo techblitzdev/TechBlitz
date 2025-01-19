7576eb429882c201f79f0b54ac7c468e2d6eb325
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-19 19:53:53 +0000

    homepage copy change

--- src/components/marketing/homepage/hero/hero.tsx
+++ src/components/marketing/homepage/hero/hero.tsx
@@ -9,10 +9,8 @@ export default async function HomepageHero() {
 
   const animatedSpanContent = (
     <div className="flex items-center group">
-      <span className="hidden md:block">
-        50% Lifetime Student Discount available
-      </span>
-      <span className="block md:hidden">50% lifetime student discount</span>
+      <span className="hidden md:block">Code editor questions out now!</span>
+      <span className="block md:hidden">Code editor questions out now!</span>
       <ArrowRight className="ml-2 size-4 group-hover:translate-x-1 transition-all duration-300" />
     </div>
   );
@@ -23,7 +21,7 @@ export default async function HomepageHero() {
       className="pb-16 pt-28 md:pb-20 md:pt-32 xl:pb-40 xl:pt-56 grid grid-cols-12 gap-4 lg:gap-16 items-center"
     >
       <div className="flex flex-col gap-y-4 col-span-full items-center text-center">
-        <Link href="/pricing">
+        <Link href="/changelog">
           <AnimatedSpan content={animatedSpanContent} />
         </Link>
         <h1 className="mt-3 text-5xl lg:text-[68px] !font-onest !font-medium tracking-tight max-w-5xl text-gradient from-white to-white/75 py-1.5">

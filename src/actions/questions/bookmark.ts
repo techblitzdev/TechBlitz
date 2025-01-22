bddcc049ff7dfda1e76c78b63d85c2e64a771809
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-22 22:59:37 +0000

    feat: action to bookmark a question created

---
+++ src/actions/questions/bookmark.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { prisma } from '@/lib/prisma';
+import { getUser } from '../user/authed/get-user';
+
+export const bookmarkQuestion = async (questionUid: string) => {
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const question = await prisma.questions.findUnique({
+    where: { uid: questionUid },
+  });
+
+  if (!question) {
+    throw new Error('Question not found');
+  }
+
+  return await prisma.users.update({
+    where: { uid: user.uid },
+    data: {
+      bookmarkedQuestions: {
+        connect: { uid: questionUid },
+      },
+    },
+  });
+};

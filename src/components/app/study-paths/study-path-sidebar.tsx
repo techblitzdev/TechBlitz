6de9b30005260d3fbd14c73dc2bff0b5f930c57d
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-25 17:39:11 +0000

    feat(study-paths): sidebar enhancements

--- src/components/app/study-paths/study-path-sidebar.tsx
+++ src/components/app/study-paths/study-path-sidebar.tsx
@@ -1,20 +1,53 @@
-import { StudyPath } from '@/utils/constants/study-paths';
+import type { StudyPath } from '@/utils/constants/study-paths';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { BookOpen, Sparkles, Target } from 'lucide-react';
 
-export default function StudyPathSidebar(opts: { studyPath: StudyPath }) {
-  const { studyPath } = opts;
-
+export default function StudyPathSidebar({
+  studyPath,
+}: {
+  studyPath: StudyPath;
+}) {
   return (
-    <aside className="w-full xl:w-1/4">
-      <div className="sticky top-10 space-y-10 w-full">
-        <div className="w-fit h-fit flex flex-col gap-y-2.5">
-          <h6 className="text-xl underline">Summary</h6>
-          <p className="text-sm text-gray-400">{studyPath.description}</p>
-        </div>
-        <div className="">
-          <h6 className="text-xl underline">Create a goal</h6>
-          <p className="text-sm text-gray-400">
-            Set a goal to complete the study path.
+    <aside className="w-full xl:w-1/4 space-y-6">
+      <div className="sticky top-10 space-y-6">
+        <div className="flex flex-col gap-y-2 border border-black-50 p-4 rounded-lg">
+          <div className="flex items-center space-x-2 text-white">
+            <BookOpen className="size-5" />
+            <span>Summary</span>
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {studyPath.description}
           </p>
+        </div>
+
+        <div className="flex flex-col gap-y-2 backdrop-blur-sm border border-black-50 p-4 rounded-lg">
+          <div className="flex items-center space-x-2 text-white">
+            <Target className="size-5" />
+            <span>Set a Goal</span>
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Set a goal to complete the study path and track your progress.
+          </p>
+          <Button className="w-full mt-2">Create Goal</Button>
+        </div>
+
+        <div className="flex flex-col gap-y-2 backdrop-blur-sm border border-black-50 p-4 rounded-lg">
+          <div className="flex items-center space-x-2 text-white">
+            <Sparkles className="size-5 text-yellow-400 fill-yellow-500" />
+            <span>Want a personalized study plan?</span>
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Upgrade to premium to get a personalized study plan to accelerate
+            your learning.
+          </p>
+          <Button
+            href="https://dub.sh/upgrade-techblitz"
+            className="mt-2 w-full"
+            variant="accent"
+          >
+            Upgrade to Premium
+          </Button>
         </div>
       </div>
     </aside>

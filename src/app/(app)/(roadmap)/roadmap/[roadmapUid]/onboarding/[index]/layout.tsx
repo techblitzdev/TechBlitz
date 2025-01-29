f8402452029392d4d7450631935e1095d821e192
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-29 20:27:33 +0000

    hotfix: remove next q in roadmap onboarding

--- src/app/(app)/(roadmap)/roadmap/[roadmapUid]/onboarding/[index]/layout.tsx
+++ src/app/(app)/(roadmap)/roadmap/[roadmapUid]/onboarding/[index]/layout.tsx
@@ -15,8 +15,11 @@ import SidebarLayoutTrigger from '@/components/app/navigation/sidebar-layout-tri
 export default async function RoadmapUidLayout({
   children,
   params,
-}: Readonly<{ children: React.ReactNode; params: { index: number } }>) {
-  const { index } = params;
+}: Readonly<{
+  children: React.ReactNode;
+  params: { index: number; roadmapUid: string };
+}>) {
+  const { index, roadmapUid } = params;
 
   const user = await useUserServer();
   if (!user) return;
@@ -30,24 +33,7 @@ export default async function RoadmapUidLayout({
           <BackToDashboard href="/roadmaps" backTo="roadmaps" />
         </div>
         <OnboardingProgressBar currentStep={index} />
-        <div className="flex items-center gap-x-5">
-          <div className="flex gap-x-2 items-center">
-            <TooltipProvider delayDuration={0} skipDelayDuration={100}>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Link
-                    href={`/roadmap/onboarding/${index + 1}`}
-                    className="bg-black-100 border border-black-50 p-2 rounded-md relative group duration-200 size-8 flex items-center justify-center"
-                  >
-                    <ChevronRight className="size-4 opacity-100 group-hover:opacity-0 absolute duration-100" />
-                    <ArrowRight className="size-4 opacity-0 group-hover:opacity-100 absolute duration-100" />
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent side="bottom">Next question</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </div>
-        </div>
+        <div className="flex items-center gap-x-5"></div>
       </div>
       <Separator className="bg-black-50 mt-5" />
       {children}

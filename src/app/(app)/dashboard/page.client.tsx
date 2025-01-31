7b8b00cfd535f7a4b5046a3b6b2afd8276ea11a9
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-01-31 19:24:36 +0000

    hotfix: upgrade modal style changes

--- src/app/(app)/dashboard/page.client.tsx
+++ src/app/(app)/dashboard/page.client.tsx
@@ -12,6 +12,8 @@ import { Button } from '@/components/ui/button';
 import { UserRecord } from '@/types/User';
 import { useRouter } from 'next/navigation';
 import ReferralToast from '@/components/shared/referral-toast';
+import { getUserDisplayName } from '@/utils/user';
+import { CheckIcon } from 'lucide-react';
 
 export default function ClientPage({
   children,
@@ -75,19 +77,58 @@ export default function ClientPage({
   return (
     <>
       <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-        <DialogContent className="sm:max-w-[425px] bg-black-100">
-          <DialogHeader>
-            <DialogTitle className="text-xl">Purchase Successful!</DialogTitle>
-            <DialogDescription className="flex flex-col gap-y-2">
-              <span>
-                You now have access to all the features on techblitz, why not
-                try one out now?
-              </span>
+        <DialogContent className="sm:max-w-3xl bg-gradient-to-b shadow-xl bg-black">
+          <DialogHeader className="space-y-2">
+            <DialogTitle className="text-2xl font-bold text-gradient from-white/55 to-white">
+              Welcome to your personalized coding experience!
+            </DialogTitle>
+            <DialogDescription className="flex flex-col gap-y-4 text-gray-200">
+              <p>
+                <span className="font-semibold">
+                  {getUserDisplayName(user)}
+                </span>
+                , thank you for purchasing the
+                <span className="font-bold"> TechBlitz Premium </span>
+                plan!
+              </p>
+              <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
+                <li className="flex items-center space-x-3">
+                  <CheckIcon className="size-6 text-accent" />
+                  <span className="text-lg font-onest">
+                    Personalized roadmaps tailored to your learning journey
+                  </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <CheckIcon className="size-6 text-accent" />
+                  <span className="text-lg font-onest">
+                    Detailed progress tracking and performance analytics
+                  </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <CheckIcon className="size-6 text-accent" />
+                  <span className="text-lg font-onest">
+                    Custom practice questions and challenges
+                  </span>
+                </li>
+                <li className="flex items-center space-x-3">
+                  <CheckIcon className="size-6 text-accent" />
+                  <span className="text-lg font-onest">
+                    Unlimited AI-assistant support
+                  </span>
+                </li>
+              </ul>
             </DialogDescription>
           </DialogHeader>
-          <DialogFooter>
-            <Button variant="secondary" href="/roadmaps">
-              Go to roadmaps
+          <DialogFooter className="flex w-full justify-end space-x-3 mt-6">
+            <Button
+              variant="default"
+              onClick={() => router.push('/dashboard')}
+              className="hover:bg-gray-700 transition-colors"
+            >
+              Go to dashboard
+            </Button>
+            <Button variant="accent" onClick={() => router.push('/roadmaps')}>
+              Explore roadmaps
             </Button>
           </DialogFooter>
         </DialogContent>

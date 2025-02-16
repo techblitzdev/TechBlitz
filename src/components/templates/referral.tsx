19b02562eebf2f4bcabb1aa7e61c79fb077b3d37
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-02-16 02:07:05 +0000

    hotfix: shortlink url

--- src/components/templates/referral.tsx
+++ src/components/templates/referral.tsx
@@ -77,7 +77,7 @@ const ReferralEmail = ({ referrerUid, referrerEmail }: ReferralEmailProps) => {
                 ></path>
               </svg>
             </Link>
-            <Link href="https://git.new/techblitz" style={socialLink}>
+            <Link href="https://git.new/blitz" style={socialLink}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"

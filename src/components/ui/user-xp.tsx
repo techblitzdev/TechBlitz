5c580527999acd71042debedee783f4d7702bb84
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-03-08 17:14:24 +0000

    minor hotfixes

--- src/components/ui/user-xp.tsx
+++ src/components/ui/user-xp.tsx
@@ -11,7 +11,7 @@ interface UserXpProps {
 async function UserXpData() {
   const { userXp } = await getUserXp();
 
-  return <p className="font-onest font-medium">{userXp}</p>;
+  return <p className="font-onest font-medium">{userXp} XP</p>;
 }
 
 export default async function UserXp({ className }: UserXpProps) {

a6c6633f9206e7c0d0199f58cc7b99dbd2109e3b
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-03-20 11:49:29 +0000

    remove .mdx files from stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,16 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {},
+  },
 };
 export default config;

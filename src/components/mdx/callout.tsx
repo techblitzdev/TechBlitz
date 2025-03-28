27a0789c4c8d1cfe8960feffc81b563b7fe9d788
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-03-28 19:48:34 +0000

    style change

--- src/components/mdx/callout.tsx
+++ src/components/mdx/callout.tsx
@@ -31,25 +31,23 @@ export default function Callout({ children, type = 'default', title, className }
         'my-6 flex gap-4 rounded-lg border p-4',
         {
           'bg-blue-200 border-blue-300 text-blue-900 dark:bg-blue-950/90 dark:border-blue-900 dark:text-blue-200':
-            type === 'default' || type === 'info',
+            type === 'default' || type === 'info' || type === 'top-tip',
           'bg-amber-200 border-amber-300 text-amber-900 dark:bg-amber-950/90 dark:border-amber-900 dark:text-amber-200':
             type === 'warning',
           'bg-green-200 border-green-300 text-green-900 dark:bg-green-950/90 dark:border-green-900 dark:text-green-200':
             type === 'success',
           'bg-red-200 border-red-300 text-red-900 dark:bg-red-950/90 dark:border-red-900 dark:text-red-200':
             type === 'error',
-          'bg-purple-200 border-purple-300 text-purple-900 dark:bg-purple-950/90 dark:border-purple-900 dark:text-purple-200':
-            type === 'top-tip',
         },
         className
       )}
     >
       <div className="mt-1 flex-shrink-0">
-        <Icon className="h-5 w-5" />
+        <Icon className="size-5" />
       </div>
       <div className="flex-1 space-y-1.5">
-        {title && <p className="font-medium">{title}</p>}
-        <div className="prose-sm prose-p:leading-relaxed prose-pre:p-0">{children}</div>
+        {title && <p className="font-medium font-onest">{title}</p>}
+        <div className="prose-sm prose-p:leading-relaxed prose-pre:p-0 font-onest">{children}</div>
       </div>
     </div>
   );

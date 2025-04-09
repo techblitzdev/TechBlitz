895ba856a09a9a4bdf17a4e9535553fde003d3e2
Logan Ford <110533855+Logannford@users.noreply.github.com>
2025-04-09 17:44:30 +0100

    feat: some layout changes to the roadmaps library.

--- src/components/app/study-paths/hero-chip.tsx
+++ src/components/app/study-paths/hero-chip.tsx
@@ -1,27 +1,28 @@
 import { Button } from '@/components/ui/button';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-import { ChevronLeft, Sparkles } from 'lucide-react';
-import type { StudyPath } from '@prisma/client';
+import { ChevronLeft } from 'lucide-react';
 
-export default function HeroChip({ studyPath }: { studyPath: StudyPath }) {
+export default function HeroChip() {
   return (
-    <div className="text-xs text-white py-1 rounded-full w-fit flex items-center gap-1 lg:gap-x-3 z-20">
+    <div className="text-xs text-white py-1 rounded-full w-fit z-20">
       <TooltipProvider>
         <Tooltip delayDuration={0}>
           <TooltipTrigger asChild>
-            <Button href="/roadmaps" variant="ghost" size="sm" className="p-1 h-fit">
+            <Button
+              href="/roadmaps"
+              variant="ghost"
+              size="sm"
+              className="p-1 h-fit flex items-center gap-1"
+            >
               <ChevronLeft className="size-4 text-white" />
+              <div className="flex items-center gap-x-1">Back to library</div>
             </Button>
           </TooltipTrigger>
           <TooltipContent>
-            <p>Back to roadmaps</p>
+            <p>Back to library</p>
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      <div className="flex items-center gap-x-1">
-        <Sparkles className="hidden md:block flex-shrink-0 size-3 text-yellow-400 fill-yellow-500" />
-        {studyPath?.heroChip}
-      </div>
     </div>
   );
 }
